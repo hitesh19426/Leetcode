672ea2d55f1d9eb124e4b74ca42926860168c8d0
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-01-03 22:26:43 +0530

    Time: 152 ms (82.64%), Space: 60.6 MB (97.72%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -6,9 +6,20 @@ public:
         
         bool possibleJudge[n+1];
         memset(possibleJudge, 1, sizeof(possibleJudge));
+        int trustCount[n+1];
+        memset(trustCount, 0, sizeof(trustCount));
+        
         for(int i=0; i<(int)trust.size(); i++){
             possibleJudge[trust[i][0]] = false;
+            trustCount[trust[i][1]]++;
         }
+        
+        for(int i=1; i<=n; i++){
+            if(possibleJudge[i] && trustCount[i] == n-1)
+                return i;
+        }
+        
+        return -1;
         
         for(int i=1; i<=n; i++){
             if(possibleJudge[i]){

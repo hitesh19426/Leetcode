c3c66dc13fd3362d8213bea0fa60889882763513
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-01-04 09:42:21 +0530

    Time: 60 ms (13.29%), Space: 19.1 MB (98.64%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,9 +3,11 @@ public:
     void moveZeroes(vector<int>& nums) {
         int to_insert = 0, n = (int)nums.size();
         for(int i=0; i<n; i++){
-            if(nums[i] != 0){
-                swap(nums[to_insert++], nums[i]);
-            }
+            if(nums[i])
+                nums[to_insert++] = nums[i];
+        }
+        while(to_insert < (int)nums.size()){
+            nums[to_insert++] = 0;
         }
     }
 };

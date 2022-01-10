6a6bb20ac08db044c8e310c4fb05b178e29b37ee
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-01-10 18:59:48 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (41.06%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -4,15 +4,20 @@ public:
         int sum = 0, carry = 0, ptr1 = a.size()-1, ptr2 = b.size()-1;
         string ans = "";
         
-        while(ptr1>=0 || ptr2>=0){
-            int x = (ptr1<0 ? 0 : a[ptr1]-'0');
-            int y = (ptr2<0 ? 0 : b[ptr2]-'0');
-            
-            ans += (x+y+carry)%2 + '0';
-            carry = (x+y+carry)/2;
-            
-            if(ptr1>=0) ptr1--;
-            if(ptr2>=0) ptr2--;
+        while(ptr1>=0 && ptr2>=0){
+            ans += (a[ptr1]-'0'+b[ptr2]-'0'+carry)%2 + '0';
+            carry = (a[ptr1]-'0'+b[ptr2]-'0'+carry)/2;
+            ptr1--, ptr2--;
+        }
+        while(ptr1>=0){
+            ans += (a[ptr1]-'0'+carry)%2 + '0';
+            carry = (a[ptr1]-'0'+carry)/2;
+            ptr1--;
+        }
+        while(ptr2>=0){
+            ans += (b[ptr2]-'0'+carry)%2 + '0';
+            carry = (b[ptr2]-'0'+carry)/2;
+            ptr2--;
         }
         if(carry){
             ans += '1';

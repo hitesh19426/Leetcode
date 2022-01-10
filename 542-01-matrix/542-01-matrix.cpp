9c0e153fc99181906dbae47fa109bfb26709e44d
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-01-11 00:16:37 +0530

    Time: 354 ms (5.01%), Space: 55.8 MB (7.95%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -2,48 +2,36 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size(),  n = mat[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 1e6));
+        vector<vector<int>> dist(m, vector<int>(n, -1));
         
+        queue<vector<int>> queue;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(mat[i][j] == 0){
-                    dp[i][j] = 0;
-                }
-                else if(i==0 && j==0){
-                    continue;
-                }
-                else if(i==0){
-                    dp[i][j] = min(dp[i][j-1]+1, dp[i][j]);
-                }
-                else if(j==0){
-                    dp[i][j] = min(dp[i][j], dp[i-1][j]+1);
-                }
-                else{
-                    dp[i][j] = min(dp[i][j], min(dp[i-1][j], dp[i][j-1])+1);
+                    queue.push({i, j, 0});
+                    dist[i][j] = 0;
                 }
             }
         }
         
-        for(int i=m-1; i>=0; i--){
-            for(int j=n-1; j>=0; j--){
-                if(mat[i][j] == 0){
-                    continue;
+        vector<int> direc{1, 0, -1, 0, 1};
+        
+        while(not queue.empty()){
+            int size = queue.size();
+            while(size--){
+                auto front = queue.front();
+                queue.pop();
+                
+                for(int i=0; i<4; i++){
+                    int x = front[0]+direc[i], y = front[1]+direc[i+1];
+                    if(x>=0 && y>=0 && x<m && y<n && dist[x][y] == -1){
+                        dist[x][y] = dist[front[0]][front[1]]+1;
+                        queue.push({x, y, dist[x][y]});
+                    }
                 }
-                else if(i==m-1 && j==n-1){
-                    continue;
-                }
-                else if(i==m-1){
-                    dp[i][j] = min(dp[i][j+1]+1, dp[i][j]);
-                }
-                else if(j==n-1){
-                    dp[i][j] = min(dp[i][j], dp[i+1][j]+1);
-                }
-                else{
-                    dp[i][j] = min(dp[i][j], min(dp[i+1][j], dp[i][j+1])+1);
-                }
-            }
+            }    
         }
         
-        return dp;
+        return dist;
     }
 };

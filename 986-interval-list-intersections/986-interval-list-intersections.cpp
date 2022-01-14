dfb978ae971991527ead7913e7de1cad693303e3
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-01-14 14:02:12 +0530

    Time: 61 ms (18.60%), Space: 18.5 MB (92.73%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.cpp
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
         vector<vector<int>> ans;
-        
-        int m = firstList.size(), n = secondList.size(), i = 0,  j = 0;
-        while(i<m && j<n){
+        int i = 0,  j = 0;
+        while(i<firstList.size() && j<secondList.size()){
             int start = max(firstList[i][0], secondList[j][0]);
             int end = min(firstList[i][1], secondList[j][1]);
             auto point = {start, end};

ba21535cccb1ba5fdefb8143b20e5d65e366a334
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-01-22 13:29:40 +0530

    Time: 44 ms (9.20%), Space: 20.1 MB (88.89%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -4,7 +4,6 @@ public:
         int s = flowerbed.size(), count = 0;
         for(int i=0; i<s; i++){
             if(flowerbed[i]==0 && (i-1<0 || flowerbed[i-1]==0) && (i+1>=s || flowerbed[i+1]==0)){
-                flowerbed[i] = 1;
                 count++;
                 i++;
             }

9c02b3216bb3deb8f596d1fab7827360763b877d
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-02-08 13:41:22 +0530

    Time: 20 ms (96.43%), Space: 22.3 MB (54.04%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -4,11 +4,11 @@ public:
         int low = 0, high = arr.size()-1, ans = -1;
         while(low <= high){
             int mid = low + (high-low)/2;
-            int index = lower_bound(arr.begin(), arr.end(), arr[mid]) - arr.begin();
+            int index = upper_bound(arr.begin(), arr.end(), arr[mid]) - arr.begin();
             if(index&1)
-                high = mid-1;
+                ans = mid, high = mid-1;
             else
-                ans = mid, low = mid+1;
+                low = mid+1;
         }
         return arr[ans];
     }

5b661eb44c39bcfda3a6aa6f0d2fa60d32c09aa2
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-02-12 13:57:56 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (51.35%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     string helper(string &s, int &i){
         string ans = "";
+        int num = 0;
         while(i < s.size() && s[i] != ']'){
             if(isalpha(s[i])){
                 ans += s[i++];
             }
             else if(isdigit(s[i])){
-                int num = 0;
+                num = 0;
                 while(isdigit(s[i])){
                     num = num*10 + (s[i]-'0');
                     i++;
                 }
-                
+            }
+            else if(s[i] == '['){
                 i++;
                 string inner = helper(s, i);
                 while(num--)

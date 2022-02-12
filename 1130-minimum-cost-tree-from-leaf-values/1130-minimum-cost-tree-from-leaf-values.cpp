725e35cd6027d57591a57f7b36b2bb5b07903cb0
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-02-12 13:31:54 +0530

    Time: 9 ms (50.63%), Space: 8.3 MB (62.68%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -7,18 +7,13 @@ public:
             int index = min_element(arr.begin(), arr.end()) - arr.begin();
             
             int sidenode;
-            if(index-1 < 0)
-                sidenode = index+1;
-            else if(index+1>=n)
-                sidenode = index-1;
-            else if(arr[index-1] < arr[index+1])
-                sidenode = index-1;
+            if(0 < index && index < n-1)
+                sidenode = (arr[index-1] < arr[index+1] ? index-1 : index+1);
             else
-                sidenode = index+1;
+                sidenode = (index ? index-1 : index+1);
             
             ans += arr[index]*arr[sidenode];
-            arr[index] = max(arr[index], arr[sidenode]);
-            arr.erase(arr.begin()+sidenode);
+            arr.erase(arr.begin()+index);
         }
         
         return ans;

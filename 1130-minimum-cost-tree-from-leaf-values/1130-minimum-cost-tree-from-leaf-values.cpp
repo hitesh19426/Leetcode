0baf9ee7984b9eaef7f9c5e10fcbf5e2177af10b
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-02-12 13:28:08 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (62.68%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,31 +1,26 @@
 class Solution {
-public:
-    int n;
-        
-    int topdown(vector<vector<int>>& dp, vector<vector<int>> &maximum, vector<int>& arr, int i, int j){
-        if(i == j)
-            return 0;
-        
-        if(dp[i][j] != INT_MAX)
-            return dp[i][j];
-        
-        int temp = INT_MAX;
-        for(int k=i; k<j; k++)
-                temp = min(temp, topdown(dp, maximum, arr, i, k) + topdown(dp, maximum, arr, k+1, j) + maximum[i][k]*maximum[k+1][j]);
-        
-        return (dp[i][j] = temp);
-    }
-    
+public: 
     int mctFromLeafValues(vector<int>& arr) {
-        n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX)), maximum(n, vector<int>(n));
-        
-        for(int i=0; i<n; i++){
-            maximum[i][i] = arr[i];
-            for(int j=i+1; j<n; j++)
-                maximum[i][j] = ::max(maximum[i][j-1], arr[j]);
+        int ans = 0;
+        while(arr.size()>1u){
+            int n = arr.size();
+            int index = min_element(arr.begin(), arr.end()) - arr.begin();
+            
+            int sidenode;
+            if(index-1 < 0)
+                sidenode = index+1;
+            else if(index+1>=n)
+                sidenode = index-1;
+            else if(arr[index-1] < arr[index+1])
+                sidenode = index-1;
+            else
+                sidenode = index+1;
+            
+            ans += arr[index]*arr[sidenode];
+            arr[index] = max(arr[index], arr[sidenode]);
+            arr.erase(arr.begin()+sidenode);
         }
         
-        return topdown(dp, maximum, arr, 0, n-1);
+        return ans;
     }
 };

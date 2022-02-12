49087eac666b263d52ed7136809321574891275a
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-02-12 13:43:12 +0530

    Time: 4 ms (76.79%), Space: 8.5 MB (41.64%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -2,19 +2,25 @@ class Solution {
 public: 
     int mctFromLeafValues(vector<int>& arr) {
         int ans = 0;
-        while(arr.size()>1u){
-            int n = arr.size();
-            int index = min_element(arr.begin(), arr.end()) - arr.begin();
-            
-            int sidenode;
-            if(0 < index && index < n-1)
-                sidenode = (arr[index-1] < arr[index+1] ? index-1 : index+1);
-            else
-                sidenode = (index ? index-1 : index+1);
-            
-            ans += arr[index]*arr[sidenode];
-            arr.erase(arr.begin()+index);
+        stack<int> stack;   // decreasing monotonic stack
+        
+        for(int node: arr){
+            while(!stack.empty() && stack.top() < node){
+                int top = stack.top();
+                stack.pop();
+                
+                ans += (stack.empty() ? top*node : top*min(node, stack.top()));
+            }
+            stack.push(node);
         }
+        
+        while(stack.size()>1u){
+            int top = stack.top();
+            stack.pop();
+            
+            ans += stack.top()*top;
+        }
+        
         
         return ans;
     }

0b584d9f1095c8e76544ea9f8a9f8135a31f9746
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-02-12 13:03:32 +0530

    Time: 12 ms (45.62%), Space: 9.4 MB (15.93%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
+    int n;
+        
+    int topdown(vector<vector<int>>& dp, vector<vector<int>> &maximum, vector<int>& arr, int i, int j){
+        if(i == j)
+            return 0;
+        
+        if(dp[i][j] != INT_MAX)
+            return dp[i][j];
+        
+        int temp = INT_MAX;
+        for(int k=i; k<j; k++)
+                temp = min(temp, topdown(dp, maximum, arr, i, k) + topdown(dp, maximum, arr, k+1, j) + maximum[i][k]*maximum[k+1][j]);
+        
+        return (dp[i][j] = temp);
+    }
+    
     int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size(), ans = 0;
+        n = arr.size();
         vector<vector<int>> dp(n, vector<int>(n, INT_MAX)), maximum(n, vector<int>(n));
         
         for(int i=0; i<n; i++){
@@ -10,22 +26,6 @@ public:
                 maximum[i][j] = ::max(maximum[i][j-1], arr[j]);
         }
         
-        
-        for(int len = 1; len<=n; len++){
-            for(int i=0; i+len-1<n; i++){
-                int j = i+len-1;
-                
-                if(i == j){
-                    dp[i][j] = 0;
-                    continue;
-                }
-                
-                for(int k=i; k<j; k++)
-                    dp[i][j] = min(dp[i][j], dp[i][k]+dp[k+1][j]+maximum[i][k]*maximum[k+1][j]);
-                
-            }
-        }
-        
-        return dp[0][n-1];
+        return topdown(dp, maximum, arr, 0, n-1);
     }
 };

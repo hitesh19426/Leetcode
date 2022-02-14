74a72423928b1ade329a14cf88e3e497923b3888
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-02-14 23:42:57 +0530

    Time: 237 ms (32.93%), Space: 12.9 MB (92.35%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int count(vector<int>& arr, int low, int high, int target){
-        int start = low-1, index = low-1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(arr[mid] < target)
-                index = mid, low=mid+1;
-            else
-                high=mid-1;
-        }
-        return index-start;
-    }
-    
     int triangleNumber(vector<int>& arr) {
         sort(arr.begin(), arr.end());
         
         int ans = 0;
-        for(int i=0; i<arr.size(); i++){
-            for(int j=i+1; j<arr.size(); j++){
-                ans += count(arr, j+1, arr.size()-1, arr[i]+arr[j]);
+        for(int i=0; i<arr.size()-1; i++){
+            if(arr[i] == 0)
+                continue;
+            int k = i+2;
+            for(int j=i+1; j<arr.size()-1; j++){
+                while(k<arr.size() && arr[k] < arr[i]+arr[j])
+                    k++;
+                ans += k-j-1;
             }
         }
         return ans;

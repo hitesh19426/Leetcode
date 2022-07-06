61c35bceeb539c8b4d6dad30618889eede64f2f5
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-06 11:48:39 +0530

    Time: 62 ms (72.53%), Space: 31.1 MB (79.36%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -8,22 +8,17 @@ public:
         vector<int> ans;
         while(right < n && left >=0 && count>0){
             if(abs(arr[left]-x) <= abs(arr[right]-x))
-                ans.push_back(arr[left--]);
+                left--;
             else
-                ans.push_back(arr[right++]);
+                right++;
             count--;
         }
         
-        while(left>=0 && count>0){
-            ans.push_back(arr[left--]);
-            count--;
-        }
-        while(right < n && count){
-            ans.push_back(arr[right++]);
-            count--;
-        }
+        while(left>=0 && count>0)
+            left--, count--;
+        while(right < n && count)
+            right++, count--;
         
-        sort(ans.begin(), ans.end());
-        return ans;
+        return vector<int> (arr.begin()+left+1, arr.begin()+right);
     }
 };

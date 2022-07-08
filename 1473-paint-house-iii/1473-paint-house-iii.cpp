dc99210054278509f8d533b0e236e7468cfa76e6
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-08 13:20:38 +0530

    Time: 184 ms (28.99%), Space: 20.6 MB (5.05%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -1,14 +1,15 @@
 class Solution {
     vector<vector<vector<int>>> dp;
+    const int INF = 1000'000'0;
     int helper(int ind, int count, int prev, vector<int>& houses, vector<vector<int>>& cost, int& m, int& n, int& target){
         if(count > target || (ind == m && count < target))
-            return 1e7;
+            return INF;
         if(ind == m)
             return 0;
         if(dp[ind][count][prev] != -1)
             return dp[ind][count][prev];
         
-        int min_cost = 1e7;
+        int min_cost = INF;
         if(houses[ind] != 0){
             bool changedNeighbor = (ind == 0 || houses[ind] != prev);
             min_cost = helper(ind+1, count + changedNeighbor, houses[ind], houses, cost, m, n, target);
@@ -24,9 +25,8 @@ class Solution {
     }
 public:
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        int curr_cost = 0, min_cost = INT_MAX, count = 0;
         dp.assign(m, vector<vector<int>>(m, vector<int>(n+1, -1)));
-        int ans = helper(0, count, 0, houses, cost, m, n, target);
-        return (ans == 1e7 ? -1 : ans);
+        int ans = helper(0, 0, 0, houses, cost, m, n, target);
+        return (ans == INF ? -1 : ans);
     }
 };

b62cbe99ebc9bb71ffa6a7f8304a4917785de6aa
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-08 19:24:30 +0530

    Time: 6 ms (57.93%), Space: 7.3 MB (38.48%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -9,7 +9,7 @@ public:
                 if(s[i-1] == '('){
                     dp[i] = (i>=2 ? dp[i-2] : 0) + 2;
                 }
-                else if(s[i-1] == ')' && i>=dp[i-1]+1 && s[i-dp[i-1]-1] == '('){
+                else if(i>=dp[i-1]+1 && s[i-dp[i-1]-1] == '('){
                     dp[i] = dp[i-1] + 2 + (i>=dp[i-1]+2 ? dp[i-dp[i-1]-2] : 0);
                 }
             }

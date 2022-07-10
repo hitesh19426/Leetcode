686fb1b5d1d89a469aa4a0d22af73cbe0980d99c
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-10 19:16:26 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (95.53%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,21 +1,40 @@
 class Solution {
-    int helper(string &s, int low, int high, int k){
-        if(high-low+1 < k)
-            return 0;
+    int findMaxSubstringWithMUniqueChar(string &s, int max_unique, int k){
+        int start = 0, end = 0, window[26] = {}, unique = 0, count_greater_equal_than_k = 0, ans = 0;
+        while(end < s.size()){
+            int windowEnd = s[end]-'a';
+            if(window[windowEnd]++ == 0)
+                unique++;
+            if(window[windowEnd] == k)
+                count_greater_equal_than_k++;
+            
+            if(unique == max_unique && count_greater_equal_than_k == unique)
+                ans = max(ans, end-start+1);
+            
+            while(unique > max_unique && start <= end){
+                int windowStart = s[start]-'a';
+                if(window[windowStart] == k)
+                    count_greater_equal_than_k--;
+                if(--window[windowStart] == 0)
+                    unique--;
+                start++;
+            }
+            
+            end++;
+        }
         
-        int count[26] = {};
-        for(int i=low; i<=high; i++)
-            count[s[i]-'a']++;
-        
-        int mid = low;
-        while(mid <= high && count[s[mid]-'a'] >= k)
-            mid++;
-        if(mid == high+1)
-            return high - low + 1;
-        return max(helper(s, low, mid-1, k), helper(s, mid+1, high, k));
+        return ans;
     }
 public:
     int longestSubstring(string s, int k) {
-        return helper(s, 0, s.size()-1, k);
+        int count[26] = {}, unique = 0, ans = 0;
+        for(int i=0; i<s.size(); i++){
+            if(count[s[i]-'a']++ == 0)
+                unique++;
+        }
+        
+        for(int i=1; i<=unique; i++)
+            ans = max(ans, findMaxSubstringWithMUniqueChar(s, i, k));
+        return ans;
     }
 };

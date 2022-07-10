7515de0260aaadecbf396f79719884fb803fbb17
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-10 23:28:38 +0530

    Time: 573 ms (8.30%), Space: 18 MB (25.15%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -1,31 +1,18 @@
-class Line{
-    int a, b, c;    // ax + by = c
-public:
-    Line(int a, int b, int c) : a(a), b(b), c(c) {}
-    bool operator<(const Line& other) const {
-        if(a != other.a)
-            return a < other.a;
-        if(b != other.b)
-            return b < other.b;
-        return c < other.c;
-    }
-    bool checkPoint(int x, int y) const {
-        return a*x + b*y == c;
-    }
-};
+bool checkPoint(const vector<int>& line, int x, int y) {
+    return line[0]*x + line[1]*y == line[2];
+}
+
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         if(points.size() <= 2)
             return points.size();
         
-        set<Line> lines;
+        set<vector<int>> lines;
         for(int i=0; i<points.size(); i++){
             for(int j=i+1; j<points.size(); j++){
                 int x1 = points[i][0], y1 = points[i][1], x2 = points[j][0], y2 = points[j][1];
-                // cout << i << " " << j << endl;
-                // cout << x1 << " " << y1 << " " << x2 << " " << y2 << endl;
-                lines.insert(Line(y1-y2, x2-x1, x2*y1-x1*y2));
+                lines.insert({y1-y2, x2-x1, x2*y1-x1*y2});
             }
         }
         
@@ -33,7 +20,7 @@ public:
         for(auto &line: lines){
             int pointOnCurrentLine = 0;
             for(int i=0; i<points.size(); i++){
-                pointOnCurrentLine += line.checkPoint(points[i][0], points[i][1]);
+                pointOnCurrentLine += checkPoint(line, points[i][0], points[i][1]);
             }
             ans = max(ans, pointOnCurrentLine);
         }

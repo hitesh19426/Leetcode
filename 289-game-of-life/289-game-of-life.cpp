36382762c5919ad03a029f2af86fc31c0c23c506
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-10 16:04:08 +0530

    Time: 2 ms (56.30%), Space: 6.8 MB (83.50%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -11,8 +11,7 @@ class Solution {
     }
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        int m = board.size(), n = board[0].size(), changed = false;
-        vector<vector<int>> copyState(board);
+        int m = board.size(), n = board[0].size();
         
         int dx[] = {1,0,-1,0,1,1,-1,-1};
         int dy[] = {0,-1,0,1,1,-1,1,-1};
@@ -21,11 +20,28 @@ public:
             for(int j=0; j<n; j++){
                 int count = 0;
                 for(int k=0; k<8; k++){
-                    if(isvalid(i+dx[k], j+dy[k], m, n) && copyState[i+dx[k]][j+dy[k]] == 1)
+                    if(isvalid(i+dx[k], j+dy[k], m, n) && abs(board[i+dx[k]][j+dy[k]]) == 1)
                         count++;
                 }
 
-                board[i][j] = find_next_state(copyState[i][j], count);
+                int newState = find_next_state(board[i][j], count);
+                if(newState != board[i][j]){
+                    if(board[i][j] == 1)
+                        board[i][j] = -1;
+                    else
+                        board[i][j] = -2;
+                }
+            }
+        }
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j] < 0){
+                    if(board[i][j] == -1)
+                        board[i][j] = 0;
+                    else
+                        board[i][j] = 1;
+                }
             }
         }
     }

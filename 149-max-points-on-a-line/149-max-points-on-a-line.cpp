d2c3df7f478679ff06e63f882694558921445470
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-10 23:57:31 +0530

    Time: 44 ms (78.14%), Space: 12.6 MB (65.10%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -1,29 +1,33 @@
-bool checkPoint(const vector<int>& line, int x, int y) {
-    return line[0]*x + line[1]*y == line[2];
-}
-
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         if(points.size() <= 2)
             return points.size();
         
-        set<vector<int>> lines;
+        int count = 0, ans = 1;
         for(int i=0; i<points.size(); i++){
+            map<pair<int, int>, int> slopes;
             for(int j=i+1; j<points.size(); j++){
                 int x1 = points[i][0], y1 = points[i][1], x2 = points[j][0], y2 = points[j][1];
-                lines.insert({y1-y2, x2-x1, x2*y1-x1*y2});
+                int gcd = __gcd(abs(y2-y1), abs(x2-x1));
+                int num = (y2-y1)/gcd, dem = (x2-x1)/gcd;
+                if(dem<0)
+                    num = -num, dem = -dem;
+                if(dem == 0)
+                    num = 1;
+                
+                pair<int, int> slope{num, dem};
+                // cout << x1 << " " << y1 << " " << x2 << " " << y2 << endl;
+                // cout << num << " " << dem << endl; 
+                if(slopes.find(slope) == slopes.end())
+                    slopes[slope] = 2;
+                else
+                    slopes[slope]++;
+                ans = max(ans, slopes[slope]);
             }
+            // cout << endl;
         }
         
-        int ans = 0;
-        for(auto &line: lines){
-            int pointOnCurrentLine = 0;
-            for(int i=0; i<points.size(); i++){
-                pointOnCurrentLine += checkPoint(line, points[i][0], points[i][1]);
-            }
-            ans = max(ans, pointOnCurrentLine);
-        }
         return ans;
     }
 };

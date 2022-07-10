abae7b484e460c3b5f933a1d11b1c17d2def4ee9
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-10 15:44:32 +0530

    Time: 4 ms (34.77%), Space: 7 MB (57.39%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    bool isvalid(int x, int y, int m, int n){
+        return x>=0 && y>=0 && x<m && y<n;
+    }
+    int find_next_state(int status, int count){
+        if(status == 0)
+            return count == 3;
+        if(count == 2 || count == 3)
+            return 1;
+        return 0;
+    }
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size(), n = board[0].size(), changed = false;
+        vector<vector<int>> nextState(m, vector<int>(n, -1));
+        
+        int dx[] = {1,0,-1,0,1,1,-1,-1};
+        int dy[] = {0,-1,0,1,1,-1,1,-1};
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int count = 0;
+                for(int k=0; k<8; k++){
+                    if(isvalid(i+dx[k], j+dy[k], m, n) && board[i+dx[k]][j+dy[k]] == 1)
+                        count++;
+                }
+
+                nextState[i][j] = find_next_state(board[i][j], count);
+            }
+        }
+        board = nextState;
+    }
+};

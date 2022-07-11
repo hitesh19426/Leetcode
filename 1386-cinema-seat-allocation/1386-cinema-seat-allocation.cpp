a8dc843eb1a623c71b39c6d6cd07fc3e4d32e914
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-11 19:20:21 +0530

    Time: 154 ms (100.00%), Space: 55.9 MB (73.22%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& arr) {
-        sort(arr.begin(), arr.end(), [](auto &a, auto &b) -> bool {
-            return a[0] < b[0];
-        });
+        unordered_map<int, int> rows;
+        for(auto& pair: arr)
+            rows[pair[0]] |= (1<<pair[1]);
         
         int groups[] = {0b111100, 0b11110000, 0b1111000000};
-        
-        int start = 0, count = 0, countRow = 0;
-        while(start < arr.size()) {
-            int row = 0, currRow = arr[start][0];
-            while(start<arr.size() && arr[start][0] == currRow)
-                row |= (1<<arr[start++][1]);
-            
+        int count = 0, countRow = 0;
+        for(auto [row, value]: rows){
             int curr = 0;
-            if((row&groups[0]) == 0)
+            if((value&groups[0]) == 0)
                 curr++;
-            if((row&groups[2]) == 0)
+            if((value&groups[2]) == 0)
                 curr++;
-            if((row&groups[1]) == 0 && curr == 0)
+            if((value&groups[1]) == 0 && curr == 0)
                 curr = 1;
             
             count += curr, countRow++;

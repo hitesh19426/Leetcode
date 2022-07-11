59f6389321963c3554a22e0059e52c31de58fa8f
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-11 19:04:15 +0530

    Time: 489 ms (14.51%), Space: 49.5 MB (98.62%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -5,31 +5,34 @@ public:
             return a[0] < b[0];
         });
         
-        int groups[][4] = {{2,3,4,5},{4,5,6,7},{6,7,8,9}};
+        int groups[] = {0b111100, 0b11110000, 0b1111000000};
+        
+        // cout << groups[0] << " " << groups[1] << " " << groups[2] << endl;
+        
         
         int start = 0, count = 0, prevRow = 0;
         while(start < arr.size()) {
             count += 2*(arr[start][0] - prevRow - 1);
             prevRow = arr[start][0];
             
-            bool row[11] = {};
+            int row = 0;
             int currRow = arr[start][0];
             while(start<arr.size() && arr[start][0] == currRow)
-                row[arr[start++][1]] = true;
+                row |= (1<<arr[start][1]), start++;
             
-            bool status[3] = {};
+            int status = 0;
             for(int i=0; i<3; i++){
-                bool done = true;
-                for(int j=0; j<4; j++){
-                    if(row[groups[i][j]])
-                        done = false;
-                }
-                status[i] = done;
+                // cout << (row&groups[i]) << " ";
+                if((row&groups[i]) == 0)
+                    status |= 1<<i;
             }
+            // cout << endl;
+            // cout << " " << row << endl;
+            // cout << row << " " << arr[start-1][0] << " " << status << endl << endl;
             
-            if(status[0] == status[2] && status[0])
+            if(status == 5 || status == 7)
                 count += 2;
-            else if(status[0] || status[1] || status[2])
+            else if(status > 0)
                 count++;
         }
         

61e7a59a500f5772f43246c14120440c4c505273
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-11 19:04:43 +0530

    Time: 353 ms (34.67%), Space: 49.7 MB (91.24%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -7,9 +7,6 @@ public:
         
         int groups[] = {0b111100, 0b11110000, 0b1111000000};
         
-        // cout << groups[0] << " " << groups[1] << " " << groups[2] << endl;
-        
-        
         int start = 0, count = 0, prevRow = 0;
         while(start < arr.size()) {
             count += 2*(arr[start][0] - prevRow - 1);
@@ -22,13 +19,9 @@ public:
             
             int status = 0;
             for(int i=0; i<3; i++){
-                // cout << (row&groups[i]) << " ";
                 if((row&groups[i]) == 0)
                     status |= 1<<i;
             }
-            // cout << endl;
-            // cout << " " << row << endl;
-            // cout << row << " " << arr[start-1][0] << " " << status << endl << endl;
             
             if(status == 5 || status == 7)
                 count += 2;

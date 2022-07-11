674b03b477d8f26ac4695a93c0de8d9059e689ff
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-11 22:43:05 +0530

    Time: 6 ms (38.65%), Space: 8.6 MB (42.18%) - LeetHub

--- 274-h-index/274-h-index.cpp
+++ 274-h-index/274-h-index.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
+        sort(citations.begin(), citations.end(), greater<int>());
         
-        int res = 0, n = citations.size();
-        for(int i=n-1; i>=0; i--){
-            if(n-1-i >= citations[i])
-                res = max(res, citations[i]);
-            if(n-i <= citations[i])
-                res = max(res, n-i);
+        int ind = 1, n = citations.size();
+        while(ind<=n && citations[ind-1] >= ind){
+            ind++;
         }
-        return res;
+        return --ind;
     }
 };

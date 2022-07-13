1c9f749a674d23d004ad085035bd298b51091ad6
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-13 18:35:58 +0530

    Time: 18 ms (17.00%), Space: 8 MB (92.00%) - LeetHub

--- 2330-valid-palindrome-iv/2330-valid-palindrome-iv.py
+++ 2330-valid-palindrome-iv/2330-valid-palindrome-iv.py
@@ -1,10 +1,14 @@
-class Solution:
-    def makePalindrome(self, s: str) -> bool:
-        low, high, count = 0, len(s)-1, 0;
-        while(low < high):
-            if(s[low] != s[high]):
-                count+=1
-            if(count > 2):
-                return False
-            low, high = low+1, high-1
-        return True
+class Solution {
+public:
+    bool makePalindrome(string s) {
+        int low = 0, high = s.size()-1, count = 0;
+        while(low < high){
+            if(s[low] != s[high])
+                count++;
+            if(count > 2)
+                return false;
+            low++, high--;
+        }
+        return true;
+    }
+};

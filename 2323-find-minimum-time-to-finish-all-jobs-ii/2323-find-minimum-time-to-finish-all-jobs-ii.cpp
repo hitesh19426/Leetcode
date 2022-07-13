0e752709623dc0fa550a547d0570d5432c60e34e
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-13 18:53:47 +0530

    Time: 328 ms (18.62%), Space: 85.9 MB (8.28%) - LeetHub

--- 2323-find-minimum-time-to-finish-all-jobs-ii/2323-find-minimum-time-to-finish-all-jobs-ii.cpp
+++ 2323-find-minimum-time-to-finish-all-jobs-ii/2323-find-minimum-time-to-finish-all-jobs-ii.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int minimumTime(vector<int>& jobs, vector<int>& workers) {
-        sort(workers.begin(), workers.end());
-        sort(jobs.begin(), jobs.end());
+        priority_queue<int> workers_pq(workers.begin(), workers.end());
+        priority_queue<int> jobs_pq(jobs.begin(), jobs.end());
         
         int ans = 0;
-        for(int i=0; i<jobs.size(); i++)
-            ans = max(ans, jobs[i]/workers[i] + (jobs[i]%workers[i] != 0));
+        while(!jobs_pq.empty()){
+            ans = max(ans, jobs_pq.top()/workers_pq.top() + (jobs_pq.top()%workers_pq.top() != 0));
+            jobs_pq.pop();
+            workers_pq.pop();
+        }
         
         return ans;
     }

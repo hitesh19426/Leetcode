8258af9cda69b063066101cf082e165f63e28c60
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-14 14:26:57 +0530

    Time: 280 ms (78.70%), Space: 99.1 MB (20.93%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,29 +1,18 @@
 class Solution {
-    int calculateMoveSuffix(int j, int ind, long long target, vector<long long>& prefix){
-        return (long long)target*(ind-j) - (prefix[ind] - prefix[j]);
-    }
-    
-    int maxEqualElementsAfterMoves(long long target, vector<long long>& prefix, int ind, int k){
-        int low = 0, high = ind-1, ans = ind;
-        while(low <= high){
-            int mid = low+(high-low)/2;
-            long long total = target*(ind-mid), left = (prefix[ind] - prefix[mid]);
-            if(total - left <= k)
-                ans = mid, high = mid-1;
-            else
-                low = mid+1;
-        }
-        return ind-ans+1;
-    }
 public:
     int maxFrequency(vector<int>& arr, int k) {
         sort(arr.begin(), arr.end());
         
-        vector<long long> prefix{0};
-        int ans = 0;
-        for(int i=0; i<arr.size(); i++){
-            prefix.push_back(prefix.back()+arr[i]);
-            ans = max(ans, maxEqualElementsAfterMoves(arr[i], prefix, i, k));
+        long long start = 0, end = 0, ans = 0;
+        long long sum = 0;
+        while(end < arr.size()){
+            sum += arr[end];
+            
+            while(arr[end]*(end-start+1) - sum > k && start <= end)
+                sum -= arr[start++];
+            
+            ans = max(ans, end-start+1);
+            end++;
         }
         return ans;
     }

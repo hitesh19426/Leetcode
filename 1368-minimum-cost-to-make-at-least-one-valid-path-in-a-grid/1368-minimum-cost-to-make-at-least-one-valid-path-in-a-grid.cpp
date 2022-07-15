ed29e611549ab94b36273312b677d43297d14f7e
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-16 00:50:04 +0530

    Time: 64 ms (82.37%), Space: 20.3 MB (50.21%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -5,32 +5,29 @@ class Solution {
 public:
     int minCost(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int>>> dist(m, vector<vector<int>>(n, vector<int>(5, INT_MAX)));
-        list<vector<int>> list;
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        list<pair<int, int>> list;
         
-        list.push_back({0, 0, grid[0][0]});
-        dist[0][0][grid[0][0]] = 0;
+        list.push_back({0, 0});
+        dist[0][0] = 0;
         
         int move[][2] = {{0,1},{0,-1},{1,0},{-1,0}};
         
         while(!list.empty()){
-            auto state = list.front();
+            auto [x, y] = list.front();
             list.pop_front();
             
-            int x = state[0], y = state[1], dir = state[2];
-            int newx = x + move[dir-1][0], newy = y + move[dir-1][1];
-            
             if(x == m-1 && y == n-1)
-                return dist[x][y][dir];
+                return dist[x][y];
             
-            if(isvalid(newx, newy, m, n) && dist[newx][newy][grid[newx][newy]] > dist[x][y][dir]){
-                dist[newx][newy][grid[newx][newy]] = dist[x][y][dir];
-                list.push_front({newx, newy, grid[newx][newy]});
-            }
-            for(int i=1; i<=4; i++){
-                if(i != dir && dist[x][y][i] > dist[x][y][dir]+1){
-                    dist[x][y][i] = dist[x][y][dir]+1;
-                    list.push_back({x, y, i});
+            for(int i=0; i<4; i++){
+                int newx = x + move[i][0], newy = y + move[i][1], weight = (i+1 != grid[x][y]);
+                if(isvalid(newx, newy, m, n) && dist[newx][newy] > dist[x][y] + weight){
+                    dist[newx][newy] = dist[x][y] + weight;
+                    if(weight == 0)
+                        list.push_front({newx, newy});
+                    else
+                        list.push_back({newx, newy});   
                 }
             }
         }

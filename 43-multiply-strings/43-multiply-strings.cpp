bc0c270b6c4128ff6bd76aed5eb78c5f74eeadf3
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-15 15:42:58 +0530

    Time: 3 ms (97.09%), Space: 6.8 MB (43.62%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int m = num1.size(), n = num2.size();
+        vector<int> prod(m+n, 0);
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                prod[i+j+1] += (num1[i]-'0')*(num2[j]-'0');
+            }
+        }
+        
+        // for(int x: prod)
+        //     cout << x << " ";
+        // cout << endl;
+        
+        int carry = 0;
+        string ans = "";
+        for(int i=m+n-1; i>=0; i--){
+            int sum = (prod[i]+carry);
+            ans += '0'+sum%10;
+            carry = sum/10;
+        }
+        
+        // cout << ans;
+        while(!ans.empty() && ans.back() == '0')
+            ans.pop_back();
+        
+        reverse(ans.begin(), ans.end());
+        
+        // while(!ans.empty() && ans.back() == '0')
+        //     ans.pop_back();
+        if(ans.empty())
+            return "0";
+        
+        // reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};

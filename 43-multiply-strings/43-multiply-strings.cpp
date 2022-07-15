59e5dde8347457a55c95eaebc4605d5f1cb0d29c
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-15 15:55:20 +0530

    Time: 9 ms (68.36%), Space: 6.6 MB (71.80%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         int m = num1.size(), n = num2.size();
-        vector<int> prod(m+n, 0);
+        vector<int> sum(m+n, 0);
         
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                prod[i+j+1] += (num1[i]-'0')*(num2[j]-'0');
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                sum[i+j+1] += (num1[i]-'0')*(num2[j]-'0');
+                sum[i+j] += sum[i+j+1]/10;
+                sum[i+j+1] %= 10;
             }
         }
 
-        int carry = 0;
         string ans = "";
         for(int i=m+n-1; i>=0; i--){
-            int sum = (prod[i]+carry);
-            ans += '0'+sum%10;
-            carry = sum/10;
+            ans += '0'+sum[i];
+            // int sum = (prod[i]+carry);
+            // ans += '0'+sum%10;
+            // carry = sum/10;
         }
         
         while(!ans.empty() && ans.back() == '0')

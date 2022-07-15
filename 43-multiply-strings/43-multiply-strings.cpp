c32b95e849b98bff27b213079c07fe750cf0983f
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-15 15:53:29 +0530

    Time: 4 ms (91.90%), Space: 6.9 MB (43.62%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -9,11 +9,7 @@ public:
                 prod[i+j+1] += (num1[i]-'0')*(num2[j]-'0');
             }
         }
-        
-        // for(int x: prod)
-        //     cout << x << " ";
-        // cout << endl;
-        
+
         int carry = 0;
         string ans = "";
         for(int i=m+n-1; i>=0; i--){
@@ -22,18 +18,12 @@ public:
             carry = sum/10;
         }
         
-        // cout << ans;
         while(!ans.empty() && ans.back() == '0')
             ans.pop_back();
-        
         reverse(ans.begin(), ans.end());
         
-        // while(!ans.empty() && ans.back() == '0')
-        //     ans.pop_back();
         if(ans.empty())
             return "0";
-        
-        // reverse(ans.begin(), ans.end());
         return ans;
     }
 };

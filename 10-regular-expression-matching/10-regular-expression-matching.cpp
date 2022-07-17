649a23bef0fa6958e0ab3facfd67d8575e29ee7e
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-17 23:22:27 +0530

    Time: 11 ms (49.94%), Space: 6.5 MB (73.03%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,49 +1,28 @@
 class Solution {
-    
-//     bool helper(string &s, string &p, int i, int j){
-//         if(i == 0 && j == 0)
-//             return dp[i][j] = true;
-//         if(j == 0)
-//             return dp[i][j] = false;
-//         if(dp[i][j] != -1)
-//             return dp[i][j];
-        
-//         if(i == 0)
-//             return dp[i][j] = (p[j-1] == '*' && helper(s,p,i,j-2));
-//         if(s[i-1] == p[j-1] || p[j-1] == '.')
-//             return dp[i][j] = helper(s,p,i-1,j-1);
-//         if(p[j-1] == '*'){
-//             dp[i][j] = helper(s,p,i,j-2) || helper(s, p, i, j-1);
-//             if(s[i-1] == p[j-2] || p[j-2] == '.')
-//                 dp[i][j] |= helper(s,p,i-1,j);
-//             return dp[i][j];
-//         }
-//         return dp[i][j] = false;
-//     }
 public:
     bool isMatch(string s, string p) {
         int m = s.size(), n = p.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, false));
+        vector<bool> prev(n+1, false);
         
-        dp[0][0] = true;
+        prev[0] = true;
         for(int j=1; j<=n; j++)
-            dp[0][j] = (p[j-1]=='*' && dp[0][j-2]);
-        
+            prev[j] = (p[j-1]=='*' && prev[j-2]);
         
         for(int i=1; i<=m; i++){
-            dp[i][0] = false;
+            vector<bool> curr(n+1, false);
             for(int j=1; j<=n; j++){
                 if(s[i-1] == p[j-1] || p[j-1] == '.')
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 else if(p[j-1] == '*'){
-                    dp[i][j] = dp[i][j-2] || dp[i][j-1];
+                    curr[j] = curr[j-2] || curr[j-1];
                     if(s[i-1] == p[j-2] || p[j-2] == '.')
-                        dp[i][j] |= dp[i-1][j];
+                        curr[j] = curr[j] || prev[j];
                 }
             }
+            prev = curr;
         }
         
-        return dp.back().back();
+        return prev.back();
     }
 };
 /*

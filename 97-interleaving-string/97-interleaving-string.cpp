b9844862f6defc48ba70a51c822d791e343e0803
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-17 23:54:30 +0530

    Time: 4 ms (70.63%), Space: 7.2 MB (35.25%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,23 +1,26 @@
 class Solution {
+    int m, n, p;
     vector<vector<int>> dp;
-    bool helper(string &s1, int i, string& s2, int j, string res, string s3, int k){
-        if(i == s1.size() && j == s2.size())
-            return (res == s3);
+    bool helper(string &s1, int i, string& s2, int j, string& s3, int k){
+        if(i == 0 && j == 0 && k == 0)
+            return true;
+        if((i == 0 && j == 0) || k == 0)
+            return false;
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        int ans = false;
-        if(i<s1.size() && s1[i] == s3[k])
-            ans |= helper(s1, i+1, s2, j, res+s1[i], s3, k+1);
-
-        if(j<s2.size() && s2[j] == s3[k])
-            ans |= helper(s1, i, s2, j+1, res+s2[j], s3, k+1);
-
-        return dp[i][j] = ans;
+        if(i == 0)
+            return dp[i][j] = (s2[j-1] == s3[k-1] && helper(s1,i,s2,j-1,s3,k-1));
+        if(j == 0)
+            return dp[i][j] = (s1[i-1] == s3[k-1] && helper(s1,i-1,s2,j,s3,k-1));
+        if(s1[i-1] == s3[k-1] || s2[j-1] == s3[k-1])
+            return dp[i][j] = (s1[i-1] == s3[k-1] && helper(s1,i-1,s2,j,s3,k-1)) || (s2[j-1] == s3[k-1] && helper(s1,i,s2,j-1,s3,k-1));
+        return dp[i][j] = false;
     }
 public:
     bool isInterleave(string s1, string s2, string s3) {
+        m = s1.size(), n = s2.size(), p = s3.size();
         dp.assign(s1.size()+1, vector<int>(s2.size()+1, -1));
-        return helper(s1, 0, s2, 0, "", s3, 0);
+        return helper(s1, m, s2, n, s3, p);
     }
 };

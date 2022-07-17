8aface1e561cc5bbd29b4b633fcf056d299de114
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-18 02:22:33 +0530

    Time: 39 ms (91.68%), Space: 10.4 MB (52.12%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minCut(string s) {
         int n = s.size();
-        vector<vector<bool>> is_palindrome(n, vector<bool>(n, false));
+        bool is_palindrome[n][n];
         for(int l=n-1; l>=0; l--){
             for(int r=l; r<n; r++){
                 if(l == r)
@@ -15,21 +15,20 @@ public:
         }
         
         vector<int> dp(n, INT_MAX);
-        dp[n-1] = 0;
-        for(int ind=n-2; ind>=0; ind--){
-            if(is_palindrome[ind][n-1]){
+        dp[0] = 0;
+        for(int ind=0; ind<n; ind++){
+            if(is_palindrome[0][ind]){
                 dp[ind] = 0;
             }
             else{
-                for(int j=ind; j<n-1; j++){
-                    if(is_palindrome[ind][j])
-                        dp[ind] = min(dp[ind], 1+dp[j+1]);
+                for(int j=1; j<=ind; j++){
+                    if(is_palindrome[j][ind])
+                        dp[ind] = min(dp[ind], 1+dp[j-1]);
                 }
             }
         }
         
-        return dp[0];
-        // return helper(n-1, is_palindrome, dp);
+        return dp[n-1];
     }
 };
 /*

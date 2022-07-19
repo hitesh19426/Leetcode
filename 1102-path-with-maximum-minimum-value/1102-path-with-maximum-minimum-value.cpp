11e4ebc21fed9ab5a3562d5f03246e48e9e2a5fb
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-19 23:05:33 +0530

    Time: 229 ms (98.96%), Space: 31.7 MB (75.98%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
@@ -3,40 +3,33 @@ class Solution {
     bool isvalid(int x, int y){
         return x>=0 && y>=0 && x<m && y<n;
     }
-    bool dfs(vector<vector<int>>& grid, vector<vector<bool>>& vis, int x, int y, int mid){    
-        if(x == m-1 && y == n-1)
-            return true;
-        
-        int dir[] = {1,0,-1,0,1};
-        vis[x][y] = true;
-        for(int k=0; k<4; k++){
-            int newx = x+dir[k], newy = y+dir[k+1];
-            if(isvalid(newx, newy) && grid[newx][newy] >= mid && !vis[newx][newy]){
-                if(dfs(grid, vis, newx, newy, mid))
-                    return true;
-            }
-        }
-        
-        return false;
-    }
 public:
     int maximumMinimumPath(vector<vector<int>>& grid) {
         m = grid.size(), n = grid[0].size();
-        int low = INT_MAX, high = min(grid[0][0], grid[m-1][n-1]), ans = -1;
-        for(auto &row: grid){
-            for(int &x: row){
-                low = min(low, x);
+        priority_queue<pair<int, int>> pq;
+        vector<vector<int>> vis(m, vector<int>(n, false));
+        
+        pq.push({grid[0][0], 0});
+        vis[0][0] = true;
+        int res = grid[0][0], dir[] = {1,0,-1,0,1};
+        
+        while(!pq.empty()){
+            auto [val, id] = pq.top();
+            pq.pop();
+                
+            res = min(res, val);
+            if(id == m*n-1)
+                return res;
+            
+            int x = id/n, y = id%n;
+            for(int k=0; k<4; k++){
+                int newx = x+dir[k], newy = y+dir[k+1];
+                if(isvalid(newx, newy) && !vis[newx][newy]){
+                    pq.push({grid[newx][newy], newx*n + newy});
+                    vis[newx][newy] = true;
+                }
             }
         }
-        
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            vector<vector<bool>> vis(m, vector<bool>(n, false));
-            if(grid[0][0] >= mid && dfs(grid, vis, 0, 0, mid))
-                ans = mid, low = mid+1;
-            else
-                high = mid-1;
-        }
-        return ans;
+        return res;
     }
 };

7e1016e8d6eb4ec5d03aebc26eb27581dafb7715
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-19 17:53:02 +0530

    Time: 145 ms (76.38%), Space: 30.7 MB (6.73%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -2,19 +2,29 @@ typedef vector<vector<long long>> vvll;
 typedef vector<long long> vll;
 
 class Solution {
-    bool check(int m, int n, vvll& prefix, int mid, int threshold){
-        for(int x1=0, x2=mid-1; x2<m; x1++, x2++){
-            for(int y1=0, y2=mid-1; y2<n; y1++, y2++){
-                long long sum = prefix[x2+1][y2+1] - prefix[x1][y2+1] - prefix[x2+1][y1] + prefix[x1][y1];
-                if(sum <= threshold)
-                    return true;
-            }
+    int m, n;
+    // bool check(int m, int n, vvll& prefix, int mid, int threshold){
+    //     for(int x1=0, x2=mid-1; x2<m; x1++, x2++){
+    //         for(int y1=0, y2=mid-1; y2<n; y1++, y2++){
+    //             long long sum = prefix[x2+1][y2+1] - prefix[x1][y2+1] - prefix[x2+1][y1] + prefix[x1][y1];
+    //             if(sum <= threshold)
+    //                 return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    
+    bool allSumGreaterThan(int start, int end, vvll& prefix, int threshold){
+        for(int i=0, j=end-start; j<n; i++, j++){
+            long long sum = prefix[end+1][j+1] - prefix[start][j+1] - prefix[end+1][i] + prefix[start][i];
+            if(sum <= threshold)
+                return false;
         }
-        return false;
+        return true;
     }
 public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
-        int m = mat.size(), n = mat[0].size();
+        m = mat.size(), n = mat[0].size();
         vvll prefix(m+1, vll(n+1, 0));
         
         for(int i=1; i<=m; i++){
@@ -23,14 +33,26 @@ public:
             }
         }
         
-        int low = 1, high = min(m, n), ans = 0;
-        while(low <= high){
-            int mid = (low+high)/2;
-            if(check(m, n, prefix, mid, threshold))
-                ans = mid, low = mid+1;
-            else
-                high = mid-1;
+        int start = 0, end = 0, ans = 0;
+        while(end < m){
+            
+            while(start <= end && allSumGreaterThan(start, end, prefix, threshold)){
+                start++;
+            }
+            
+            ans = max(ans, end-start+1);
+            end++;
         }
+        
+        
+        // int low = 1, high = min(m, n), ans = 0;
+        // while(low <= high){
+        //     int mid = (low+high)/2;
+        //     if(check(m, n, prefix, mid, threshold))
+        //         ans = mid, low = mid+1;
+        //     else
+        //         high = mid-1;
+        // }
         return ans;
     }
 };

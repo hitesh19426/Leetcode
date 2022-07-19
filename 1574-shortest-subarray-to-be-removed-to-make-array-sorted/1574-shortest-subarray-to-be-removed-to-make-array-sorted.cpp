4261bb94888eb81da718b44e8bbcc84ddcbf7811
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-19 16:55:16 +0530

    Time: 264 ms (11.58%), Space: 71.4 MB (6.00%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -4,33 +4,25 @@ public:
         if(is_sorted(arr.begin(), arr.end()))
             return 0;
         
-        int n = arr.size();
-        vector<pair<bool, int>> prefix(arr.size()+1);
-        prefix[0] = {true, INT_MIN};
-        for(int i=0; i<arr.size(); i++){
-            bool prefix_is_sorted = (prefix[i].first && arr[i] >= prefix[i].second);
-            int prefix_max = max(prefix[i].second, arr[i]);
-            prefix[i+1] = {prefix_is_sorted, prefix_max};
-        }
-        
         vector<pair<bool, int>> suffix(arr.size()+1);
-        suffix[n] = {true, INT_MAX};
+        suffix[arr.size()] = {true, INT_MAX};
         for(int i=arr.size()-1; i>=0; i--){
             bool suffix_is_sorted = (suffix[i+1].first && suffix[i+1].second >= arr[i]);
             int suffix_min = min(suffix[i+1].second, arr[i]);
             suffix[i] = {suffix_is_sorted, suffix_min};
         }
         
-        int start = 0, end = 0, ans = INT_MAX;
+        int start = 0, end = 0, ans = INT_MAX, pre_max = INT_MIN, issorted = true;
         while(end < arr.size()){
-            
-            while(suffix[end+1].first && prefix[start].first && prefix[start].second <= suffix[end+1].second && start <= end){
+            while(suffix[end+1].first && issorted && pre_max <= suffix[end+1].second && start <= end){
                 ans = min(ans, end-start+1);
+                
+                issorted &= (arr[start] >= pre_max);
+                pre_max = max(pre_max, arr[start]);
                 start++;
             }
             
             end++;
-                
         }
         return ans;
     }

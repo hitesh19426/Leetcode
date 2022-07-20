5e973e6d892a48ed3aed10d0b7dfd86439b65337
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-20 11:57:28 +0530

    Time: 268 ms (72.73%), Space: 46 MB (80.62%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
-        vector<vector<int>> index(26, vector<int>());
-        for(int i=0; i<s.size(); i++){
-            index[s[i]-'a'].push_back(i);
+        vector<vector<pair<int, int>>> indexes(26, vector<pair<int, int>>());
+        for(int i=0; i<words.size(); i++){
+            indexes[words[i][0]-'a'].push_back({i, 0});
         }
         
         int count = 0;
-        for(auto &word: words){
-            int ind = -1, done = true;
-            for(auto &c: word){
-                auto itr = upper_bound(index[c-'a'].begin(), index[c-'a'].end(), ind);
-                if(itr == index[c-'a'].end()){
-                    done = false;
-                    break;
-                }
-                ind = *itr;
+        for(auto &c: s){
+            auto curr = indexes[c-'a'];
+            indexes[c-'a'].clear();
+            
+            for(auto &[index, letter]: curr){
+                auto &word = words[index];
+                if(letter+1 == word.size())
+                    count++;
+                else
+                    indexes[word[letter+1]-'a'].push_back({index, letter+1});
             }
-            if(done)
-                count++;
         }
         
         return count;

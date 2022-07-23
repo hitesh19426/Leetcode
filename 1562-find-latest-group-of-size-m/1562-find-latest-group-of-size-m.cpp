23d33fb3cfe5cca5b7af67872cba2fc57223f59f
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-23 15:11:17 +0530

    Time: 338 ms (38.42%), Space: 86 MB (37.93%) - LeetHub

--- 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
+++ 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
@@ -1,59 +1,68 @@
 class Solution {
     int target, count = 0;
-    vector<int> parent, size;
+    vector<int> parent, size, left, right;
 	void make_set(int v){
-		parent[v] = v;
+		// parent[v] = v;
+        left[v] = right[v] = v;
 		size[v] = 1;
         if(target == 1)
             count++;
 	}
 
-	int find_set(int v){
-		if(v == parent[v])
-			return v;
-		return parent[v] = find_set(parent[v]);
-	}
+	// int find_set(int v){
+	// 	if(v == parent[v])
+	// 		return v;
+	// 	return parent[v] = find_set(parent[v]);
+	// }
 
 	void union_set(int a, int b){
-		a = find_set(a);
-		b = find_set(b);
-		if(a != b){
-			if(size[a] < size[b])
-				swap(a, b);
-			
-            if(target == size[a])
-                count--;
-            if(target == size[b])
-                count--;
-            
-            parent[b] = a;
-            size[a] += size[b];
-            
-            if(target == size[a])
-                count++;
-		}
+		// a = find_set(a);
+		// b = find_set(b);
+        // left[b] = left[a];
+        // right[a] = b;
+        
+        // if(size[a] < size[b])
+        //     swap(a, b);
+        
+        left[right[b]] = left[a];
+        right[left[a]] = right[b];
+        if(target == size[a])
+            count--;
+        if(target == size[b])
+            count--;
+
+        // parent[b] = a;
+        int total = size[a] + size[b];
+        size[left[a]] = size[right[b]] = total;
+        // size[left[a]] += size[b];
+
+        if(target == total)
+            count++;
 	}
     
-    bool find_size(int m){
+    bool find_size(){
         return count;
     }
 public:
     int findLatestStep(vector<int>& arr, int m) {
         int n = arr.size();
         target = m;
-        parent.assign(n, -1);
+        vector<bool> seen(n, false);
+        left.assign(n, -1); right.assign(n, -1);
         size.assign(n, 0);
         
         int i = 0, ans = -1;
         for(int x: arr){
             x--;
+            seen[x] = true;
             make_set(x);
             
-            if(x-1>=0 && parent[x-1] != -1)
+            
+            if(x-1>=0 && seen[x-1])
                 union_set(x-1, x);
-            if(x+1<arr.size() && parent[x+1] != -1)
-                union_set(x+1, x);
-            if(find_size(m))
+            if(x+1<arr.size() && seen[x+1])
+                union_set(x, x+1);
+            if(count)
                 ans = max(ans, i+1);
             i++;
         }

c7cb94086c4ca8bd5c706d32676ccc19fdfd175d
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-24 13:45:27 +0530

    Time: 555 ms (39.73%), Space: 109.9 MB (81.69%) - LeetHub

--- 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.cpp
+++ 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
-        map<int, int> lastfull;
-        set<int> zeroes, seen;
+        unordered_map<int, int> lastfull;
+        set<int> zeroes;
         vector<int> ans(rains.size(), -1);
         
         for(int i=0; i<rains.size(); i++){
             if(rains[i] == 0){
                 zeroes.insert(i);
             }
-            else if(lastfull.find(rains[i]) == lastfull.end()){
-                lastfull[rains[i]] = i;
-            }
             else{
-                int last = lastfull[rains[i]];
-                auto zero = zeroes.upper_bound(last);
-                if(zero == zeroes.end())
-                    return {};
-                ans[*zero] = rains[i];
-                zeroes.erase(zero);
+                if(lastfull.find(rains[i]) != lastfull.end()){
+                    int last = lastfull[rains[i]];
+                    auto zero = zeroes.upper_bound(last);
+                    if(zero == zeroes.end())
+                        return {};
+                    ans[*zero] = rains[i];
+                    zeroes.erase(zero);
+                }
                 lastfull[rains[i]] = i;
-            }
+            } 
         }
         for(auto zero: zeroes){
             ans[zero] = 1;

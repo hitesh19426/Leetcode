d6d699fd02a628ccf8915415d83d797d66eacfe3
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-26 15:18:42 +0530

    Time: 718 ms (5.07%), Space: 89.9 MB (5.06%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    vector<vector<int>> dp;
+    // map<pair<int, int>, int> dp;
+    int helper(int ind, int last, vector<int>& prices){
+        if(ind >= prices.size())
+            return 0;
+        // if(dp[{ind, last}] != dp.end())
+        //     return dp[{ind, last}];
+        if(dp[ind][last+1] != -1)
+            return dp[ind][last+1];
+        
+        if(last == -1)
+            return dp[ind][last+1] = max(helper(ind+1, ind, prices), helper(ind+1, -1, prices));
+        return dp[ind][last+1] = max(prices[ind]-prices[last] + helper(ind+2, -1, prices), helper(ind+1, last, prices));
+        
+        // if(last == -1)
+        //     return dp[{ind, last}] = max(helper(ind+1, ind, prices), helper(ind+1, -1, prices));
+        // return dp[{ind, last}] = max(arr[ind]-arr[last]+helper(ind+2, -1, prices), helper(ind+1, last, prices));
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        dp.assign(prices.size(), vector<int>(prices.size()+1, -1));
+        return helper(0, -1, prices);
+    }
+};

7bcbd53b92f2a796b66e4741f45cbe5e8c4265bd
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-27 12:20:19 +0530

    Time: 24 ms (6.22%), Space: 26.7 MB (40.60%) - LeetHub

---
+++ 593-valid-square/593-valid-square.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int distance(vector<int>& pt1, vector<int>& pt2){
+        return ((pt1[0] - pt2[0])*(pt1[0] - pt2[0]) + (pt1[1] - pt2[1])*(pt1[1] - pt2[1]));
+    }
+public:
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        vector<vector<int>> points{p1, p2, p3, p4};
+        
+        for(auto pt1: points){
+            map<int, int> dist;
+            for(auto pt2: points){
+                if(pt1 != pt2){
+                    dist[distance(pt1, pt2)]++;
+                }
+            }
+            if(dist.size() != 2 || dist.begin()->second != 2)
+                return false;
+            if(dist.rbegin()->first != 2*dist.begin()->first)
+                return false;
+        }
+        
+        return true;
+    }
+};

308cbd8a76a5fbc34df55263121d1533f770db41
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-27 13:00:15 +0530

    Time: 395 ms (43.98%), Space: 126.7 MB (29.36%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -9,39 +9,18 @@
  * };
  */
 class Solution {
-    ListNode* reverse(ListNode* head){
-        ListNode* prev = NULL, *curr = head;
-        while(curr){
-            auto next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-    
-    ListNode* middle(ListNode* head){
-        auto slow = head, fast = head->next;
-        while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
 public:
     int pairSum(ListNode* head) {
-        ListNode* mid = middle(head);
-        ListNode* secondList = mid->next;
-        mid->next = NULL;
-        
-        secondList = reverse(secondList);
+        vector<int> arr;
+        auto ptr = head;
+        while(ptr){
+            arr.push_back(ptr->val);
+            ptr = ptr->next;
+        }
         
         int ans = 0;
-        while(head){
-            ans = max(head->val + secondList->val, ans);
-            head = head->next;
-            secondList = secondList->next;
-        }
+        for(int low = 0, high = arr.size()-1; low < high; low++, high--)
+            ans = max(arr[low] + arr[high], ans);
         return ans;
     }
 };

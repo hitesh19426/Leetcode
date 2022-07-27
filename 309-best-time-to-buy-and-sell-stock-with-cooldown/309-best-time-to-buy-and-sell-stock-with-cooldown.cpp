164ba280d19b52c113fbbbc94bc55e30e1601c3c
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-28 00:07:16 +0530

    Time: 204 ms (5.07%), Space: 11.7 MB (51.49%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,18 +1,23 @@
 class Solution {
-    vector<vector<int>> dp;
-    int helper(int ind, int last, vector<int>& prices){
+    vector<int> dp;
+    int helper(int ind, vector<int>& prices){
         if(ind >= prices.size())
             return 0;
-        if(dp[ind][last+1] != -1)
-            return dp[ind][last+1];
+        if(dp[ind] != -1)
+            return dp[ind];
         
-        if(last == -1)
-            return dp[ind][last+1] = max(helper(ind+1, ind, prices), helper(ind+1, -1, prices));
-        return dp[ind][last+1] = max(prices[ind]-prices[last] + helper(ind+2, -1, prices), helper(ind+1, last, prices));
+        int ans = helper(ind+1, prices);
+        for(int i=ind+1; i<prices.size(); i++)
+            ans = max(ans, prices[i] - prices[ind] + helper(i+2, prices));
+        return dp[ind] = ans;
+        
+        // if(last == -1)
+        //     return dp[ind][last+1] = max(helper(ind+1, ind, prices), helper(ind+1, -1, prices));
+        // return dp[ind][last+1] = max(prices[ind]-prices[last] + helper(ind+2, -1, prices), helper(ind+1, last, prices));
     }
 public:
     int maxProfit(vector<int>& prices) {
-        dp.assign(prices.size(), vector<int>(prices.size()+1, -1));
-        return helper(0, -1, prices);
+        dp.assign(prices.size()+1, -1);
+        return helper(0, prices);
     }
 };

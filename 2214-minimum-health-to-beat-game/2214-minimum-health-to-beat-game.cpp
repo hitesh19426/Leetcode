ad3ff52e4d7caae1c3046167d0fce3c338ce7226
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-27 14:58:34 +0530

    Time: 162 ms (35.12%), Space: 59.2 MB (64.21%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.cpp
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.cpp
@@ -1,23 +1,11 @@
 class Solution {
-    bool check(vector<int>& arr, int armor, long health){
-        health -= max(arr[0]-armor, 0);
-        for(int i=1; i<arr.size(); i++){
-            health -= arr[i];
-        }
-        return health>0;
-    }
 public:
     long long minimumHealth(vector<int>& damage, int armor) {
         sort(damage.begin(), damage.end(), greater<int>());
-        
-        long low = 0, high = accumulate(damage.begin(), damage.end(), 0ll)+1, ans = -1;
-        while(low <= high){
-            long mid = (low+high)/2;
-            if(check(damage, armor, mid))
-                ans = mid, high = mid-1;
-            else
-                low = mid+1;
-        }
-        return ans;
+        long long health = max(damage[0]-armor, 0);
+        for(int i=1; i<damage.size(); i++){
+            health += damage[i];
+        }        
+        return health+1;
     }
 };

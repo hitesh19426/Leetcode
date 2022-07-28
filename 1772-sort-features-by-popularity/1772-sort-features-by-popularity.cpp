d1b977448bc8d308531ad8b7071839fd7edf28f4
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-28 20:55:25 +0530

    Time: 657 ms (52.63%), Space: 81 MB (29.82%) - LeetHub

--- 1772-sort-features-by-popularity/1772-sort-features-by-popularity.cpp
+++ 1772-sort-features-by-popularity/1772-sort-features-by-popularity.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
     vector<string> sortFeatures(vector<string>& features, vector<string>& responses) {
-        unordered_set<string> dict(features.begin(), features.end());   // make dict of features so we search features easily
-        unordered_map<string, int> count;   // feature, count
+        unordered_set<string> dict(features.begin(), features.end());
+        unordered_map<string, int> count;
         
         for(auto &response: responses){
             unordered_set<string> unique_words_in_response;
-            stringstream ss(response);      // use string stream to separate words
+            stringstream ss(response);
             string word;
             
             while(ss >> word){
-                unique_words_in_response.insert(word);  // insert all words from response into a set to get unique words
+                unique_words_in_response.insert(word);
             }
             
-            // for each word, check if there is a feature with same name, if it is, increase count of that feature
             for(auto &word: unique_words_in_response){
                 if(dict.find(word) != dict.end())
                     count[word]++;
             }
         }
         
-        vector<vector<int>> buckets(responses.size()+1);
-        for(int i=0; i<features.size(); i++)
-            buckets[count[features[i]]].push_back(i);
+        stable_sort(features.begin(), features.end(), [&count](const auto &a, const auto &b) -> bool {
+            return count[a] > count[b];
+        });
         
-        vector<string> ans;
-        for(int i=buckets.size()-1; i>=0; i--){
-            for(int& ind: buckets[i])
-                ans.push_back(features[ind]);
-        }
-        
-        return ans;
+        return features;
     }
 };
-/*
-TC(F, R, MFL, MRL) = F*MFL + R*(MRL) + F*R
-SC(F, R, MFL, MRL) = F*MFL + R*(MRL)
-*/

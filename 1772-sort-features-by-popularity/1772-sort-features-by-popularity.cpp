8fd1e0f46e4679d93116f2a0e35d1b1f1eefe9a5
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-28 20:33:36 +0530

    Time: 640 ms (56.14%), Space: 81.6 MB (29.82%) - LeetHub

--- 1772-sort-features-by-popularity/1772-sort-features-by-popularity.cpp
+++ 1772-sort-features-by-popularity/1772-sort-features-by-popularity.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     vector<string> sortFeatures(vector<string>& features, vector<string>& responses) {
-        unordered_set<string> dict(features.begin(), features.end());
-        unordered_map<string, int> count;
+        unordered_set<string> dict(features.begin(), features.end());   // make dict of features so we search features easily
+        unordered_map<string, int> count;   // feature, count
         
         for(auto &response: responses){
             unordered_set<string> unique_words_in_response;
-            stringstream ss(response);
+            stringstream ss(response);      // use string stream to separate words
             string word;
             
             while(ss >> word){
-                unique_words_in_response.insert(word);
+                unique_words_in_response.insert(word);  // insert all words from response into a set to get unique words
             }
             
+            // for each word, check if there is a feature with same name, if it is, increase count of that feature
             for(auto &word: unique_words_in_response){
                 if(dict.find(word) != dict.end())
                     count[word]++;
             }
         }
         
+        // sort features in non-increasing order based on count -> stable sort automatically handles indexing
         stable_sort(features.begin(), features.end(), [&count](const auto &a, const auto &b) -> bool {
             return count[a] > count[b];
         });
@@ -26,3 +28,7 @@ public:
         return features;
     }
 };
+/*
+TC(F, R, MFL, MRL) = F*MFL + R*(MRL) + F*logF*(MFL)
+SC(F, R, MFL, MRL) = F*MFL + R*(MRL)
+*/

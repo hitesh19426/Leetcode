f3d1bbd4e8b5f5ac97431684ba7a18bcfd3571cd
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-29 18:23:56 +0530

    Time: 355 ms (11.96%), Space: 93.4 MB (83.21%) - LeetHub

--- 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.cpp
+++ 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.cpp
@@ -1,7 +1,7 @@
 class Solution {
-    void fillCount(vector<int>& arr, vector<int>& count){
-        for(int& x: arr){
-            for(int i=0; i<31; i++){
+    void fillCount(vector<int>& arr, int count[32]){
+        for(int i=0; i<31; i++){
+            for(int& x: arr){
                 if(x&(1<<i))
                     count[i]++;
             }
@@ -9,9 +9,7 @@ class Solution {
     }
 public:
     int getXORSum(vector<int>& arr1, vector<int>& arr2) {
-        long long m = arr1.size(), n = arr2.size();
-        vector<int> count1(32, 0), count2(32, 0);
-        
+        int count1[32] = {}, count2[32] = {};
         fillCount(arr1, count1);
         fillCount(arr2, count2);
         

490a92c1946a190490fe0004559586fb954b7231
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-29 18:02:55 +0530

    Time: 524 ms (76.16%), Space: 131.4 MB (92.05%) - LeetHub

--- 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -5,47 +5,28 @@ public:
         priority_queue<int> left(arr.begin(), arr.begin()+k);
         long long left_sum = accumulate(arr.begin(), arr.begin()+k, 0ll);
 
-        map<int, int> higher, lower;
-        for(int i=k; i<2*k; i++)
-            higher[arr[i]]++;
-        long long high_sum = accumulate(arr.begin()+k, arr.begin()+2*k, 0ll);
-
-        for(int i=2*k; i<n; i++){
-            higher[arr[i]]++;
-            auto itr = higher.begin();
-            high_sum += arr[i] - itr->first;
-            
-            lower[itr->first]++;
-            if(--higher[itr->first] == 0)
-                higher.erase(itr);
-        }
-
-        long long ans = left_sum - high_sum;
+        long long min_sum[n], max_sum[n];
+        min_sum[k-1] = left_sum;
         for(int i=k; i<2*k; i++){
-            if(lower.find(arr[i]) != lower.end()){
-                if(--lower[arr[i]] == 0)
-                    lower.erase(arr[i]);
-            }
-            else{
-                high_sum -= arr[i];
-                
-                auto itr = lower.rbegin();
-                if(--higher[arr[i]] == 0)
-                    higher.erase(arr[i]);
-
-                high_sum += itr->first;
-                higher[itr->first]++;
-                if(--lower[itr->first] == 0)
-                    lower.erase(itr->first);
-            }
-
             left.push(arr[i]);
-            left_sum += arr[i] -  left.top();
+            left_sum += arr[i] - left.top();
             left.pop();
-
-            ans = min(ans, left_sum - high_sum);
+            min_sum[i] = left_sum;
         }
-
+        
+        priority_queue<int, vector<int>, greater<int>> right(arr.begin()+2*k, arr.end());
+        long long right_sum = accumulate(arr.begin()+2*k, arr.end(), 0ll);
+        max_sum[2*k] = right_sum;
+        for(int i=2*k-1; i>=k; i--){
+            right.push(arr[i]);
+            right_sum += arr[i] - right.top();
+            right.pop();
+            max_sum[i] = right_sum;
+        }
+        
+        long long ans = LLONG_MAX;
+        for(int i=k-1; i<2*k; i++)
+            ans = min(ans, min_sum[i] - max_sum[i+1]);
         return ans;
     }
 };

2fa5762eba95389bb29199f43b0b404c2536b0a9
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-29 17:47:02 +0530

    Time: 646 ms (55.07%), Space: 131.4 MB (92.05%) - LeetHub

--- 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -5,32 +5,38 @@ public:
         priority_queue<int> left(arr.begin(), arr.begin()+k);
         long long left_sum = accumulate(arr.begin(), arr.begin()+k, 0ll);
 
-        multiset<int> higher(arr.begin()+k, arr.begin()+2*k), lower;
-        long long high_sum = accumulate(arr.begin()+k, arr.begin()+2*k, 0ll), low_sum = 0ll;
+        map<int, int> higher, lower;
+        for(int i=k; i<2*k; i++)
+            higher[arr[i]]++;
+        long long high_sum = accumulate(arr.begin()+k, arr.begin()+2*k, 0ll);
 
         for(int i=2*k; i<n; i++){
-            higher.insert(arr[i]);
-            high_sum += arr[i];
-
-            lower.insert(*higher.begin());
-            low_sum += *higher.begin();
-
-            high_sum -= *higher.begin();
-            higher.erase(higher.begin());
+            higher[arr[i]]++;
+            auto itr = higher.begin();
+            high_sum += arr[i] - itr->first;
+            
+            lower[itr->first]++;
+            if(--higher[itr->first] == 0)
+                higher.erase(itr);
         }
 
         long long ans = left_sum - high_sum;
         for(int i=k; i<2*k; i++){
             if(lower.find(arr[i]) != lower.end()){
-                lower.erase(lower.find(arr[i]));
+                if(--lower[arr[i]] == 0)
+                    lower.erase(arr[i]);
             }
             else{
                 high_sum -= arr[i];
-                higher.erase(higher.find(arr[i]));
+                
+                auto itr = lower.rbegin();
+                if(--higher[arr[i]] == 0)
+                    higher.erase(arr[i]);
 
-                high_sum += *lower.rbegin();
-                higher.insert(*lower.rbegin());
-                lower.erase(lower.find(*lower.rbegin()));
+                high_sum += itr->first;
+                higher[itr->first]++;
+                if(--lower[itr->first] == 0)
+                    lower.erase(itr->first);
             }
 
             left.push(arr[i]);

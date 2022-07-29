68ad25255235534624039e9023ea9cc4dbb061e7
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-29 18:54:58 +0530

    Time: 15 ms (67.52%), Space: 6 MB (32.96%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        int ans = 0, diff = right - left;
+        int ans = 0, diff = right - left, andlr = left&right;
         for(int i=0; i<31; i++){
-            bool bit1 = left&1, bit2 = right&1, bit3 = (diff <= (1<<i)-1);
-            if(bit1 && bit2 && bit3)
+            bool bit3 = (diff <= (1<<i)-1);
+            if(andlr&1 && (diff <= ((1<<i)-1)))
                 ans |= (1<<i);
-            left >>= 1, right >>= 1;
+            andlr >>= 1;
         }
         return ans;
     }

a124fc999efb2145b7ffff59a2e391ac86709e18
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-29 18:09:12 +0530

    Time: 690 ms (49.59%), Space: 129.1 MB (94.52%) - LeetHub

--- 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -2,18 +2,8 @@ class Solution {
 public:
     long long minimumDifference(vector<int>& arr) {
         int n = arr.size(), k = n/3;
-        priority_queue<int> left(arr.begin(), arr.begin()+k);
-        long long left_sum = accumulate(arr.begin(), arr.begin()+k, 0ll);
-
-        long long min_sum[n], max_sum[n];
-        min_sum[k-1] = left_sum;
-        for(int i=k; i<2*k; i++){
-            left.push(arr[i]);
-            left_sum += arr[i] - left.top();
-            left.pop();
-            min_sum[i] = left_sum;
-        }
         
+        long long max_sum[n];
         priority_queue<int, vector<int>, greater<int>> right(arr.begin()+2*k, arr.end());
         long long right_sum = accumulate(arr.begin()+2*k, arr.end(), 0ll);
         max_sum[2*k] = right_sum;
@@ -22,11 +12,18 @@ public:
             right_sum += arr[i] - right.top();
             right.pop();
             max_sum[i] = right_sum;
-        }
+        }        
         
-        long long ans = LLONG_MAX;
-        for(int i=k-1; i<2*k; i++)
-            ans = min(ans, min_sum[i] - max_sum[i+1]);
+        priority_queue<int> left(arr.begin(), arr.begin()+k);
+        long long left_sum = accumulate(arr.begin(), arr.begin()+k, 0ll);
+        
+        long long ans = left_sum - max_sum[k];
+        for(int i=k; i<2*k; i++){
+            left.push(arr[i]);
+            left_sum += arr[i] - left.top();
+            left.pop();
+            ans = min(ans, left_sum - max_sum[i+1]);
+        }
         return ans;
     }
 };

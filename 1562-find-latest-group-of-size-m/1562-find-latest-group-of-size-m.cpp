10d7a35b9a73ea09237e94b85282c22be48c4ce9
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-07-30 14:54:15 +0530

    Time: 254 ms (63.09%), Space: 79.5 MB (89.53%) - LeetHub

--- 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
+++ 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
@@ -1,53 +1,24 @@
 class Solution {
-    int target, count = 0;
-	void make_set(int v, int left[], int right[], int size[]){
-        left[v] = right[v] = v;
-		size[v] = 1;
-        if(target == 1)
-            count++;
-	}
-    
-	void union_set(int a, int b, int left[], int right[], int size[]){
-        left[right[b]] = left[a];
-        right[left[a]] = right[b];
-        if(target == size[a])
-            count--;
-        if(target == size[b])
-            count--;
-
-        int total = size[a] + size[b];
-        size[left[a]] = size[right[b]] = total;
-
-        if(target == total)
-            count++;
-	}
 public:
     int findLatestStep(vector<int>& arr, int m) {
         int n = arr.size();
-        target = m;
-        vector<bool> seen(n, false);
-        int left[n], right[n], size[n];
+        vector<int> size(n+2, 0);
         
-        int i = 0, ans = -1;
+        int ans = -1, step = 1, count = 0;
         for(int x: arr){
-            x--;
-            seen[x] = true;
-            make_set(x, left, right, size);
-            
-            
-            if(x-1>=0 && seen[x-1])
-                union_set(x-1, x, left, right, size);
-            if(x+1<arr.size() && seen[x+1])
-                union_set(x, x+1, left, right, size);
+            int left = x - size[x-1], right = x + size[x+1];
+            count -= ((size[left] == m) + (size[right] == m)); 
+            size[left] = size[right] = size[x] = size[left] + size[right] + 1;
+            count += (size[x] == m);
             if(count)
-                ans = max(ans, i+1);
-            i++;
+                ans = max(ans, step);
+            step++;
         }
         
         return ans;
     }
 };
 /*
-TC(N, M) = O(N*alpha(N))
+TC(N, M) = O(N))
 SC(N, M) = O(N)
 */

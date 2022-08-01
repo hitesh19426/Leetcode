aebc44b5b541dac0dc6caf02b71e463887754235
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-01 22:51:05 +0530

    Time: 81 ms (9.80%), Space: 26 MB (8.92%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m = str1.size(), n = str2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        vector<vector<pair<int, int>>> parent(m+1, vector<pair<int, int>>(n+1, {-1, -1}));
+        
+        dp[0][0] = 0;
+        for(int i=1; i<=m; i++){
+            parent[i][0] = {i-1, 0};
+            dp[i][0] = i;
+        }
+        for(int j=1; j<=n; j++){
+            parent[0][j] = {0, j-1};
+            dp[0][j] = j;
+        }
+        
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    parent[i][j] = {i-1, j-1};
+                }
+                else if(dp[i-1][j] < dp[i][j-1]){
+                    dp[i][j] = dp[i-1][j]+1;
+                    parent[i][j] = {i-1, j};
+                }
+                else{
+                    dp[i][j] = dp[i][j-1] + 1;
+                    parent[i][j] = {i, j-1};
+                }
+            }
+        }
+        
+        int x = m, y = n;
+        string ans = "";
+        while(x != 0 || y != 0){
+            auto &p = parent[x][y];
+            if(p.first == x || p.second != y)
+                ans += str2[y-1];        
+            else
+                ans += str1[x-1];
+            x = p.first, y = p.second;
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+/*
+"bbbaaaba"
+"bbababbb"
+*/

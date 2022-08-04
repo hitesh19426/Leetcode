fe6734a00ed3c78e8398c2a563e06d32d2c0b8ba
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-04 18:13:52 +0530

    Time: 68 ms (29.67%), Space: 17.2 MB (43.96%) - LeetHub

--- 356-line-reflection/356-line-reflection.cpp
+++ 356-line-reflection/356-line-reflection.cpp
@@ -12,9 +12,9 @@ public:
             for(auto &x: pair.second)
                 arr.push_back(x);
             int low = 0, high = arr.size()-1;
+            if(y == INT_MAX)
+                y = arr[low] + arr[high];
             while(low <= high){
-                if(y == INT_MAX)
-                    y = (arr[low]+arr[high]);
                 if(y != arr[low] + arr[high])
                     return false;
                 low++, high--;

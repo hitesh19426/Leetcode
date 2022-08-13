5878ab6abd6f261554190a9631aa6757bf16e300
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-13 13:58:45 +0530

    Time: 7 ms (93.67%), Space: 7.9 MB (79.55%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -9,25 +9,30 @@ public:
                 curr = 10*curr + (s[i]-'0');
             if(i == s.size()-1 || (!isdigit(s[i]) && !isspace(s[i]))){
                 if(lastsign == '+'){
-                    stack.push(curr);
+                    // stack.push(curr);
+                    res += last, last = curr;
                 }else if(lastsign == '-'){
-                    stack.push(-curr);
+                    // stack.push(-curr);
+                    res += last, last = -curr;
                 }else if(lastsign == '*'){
-                    int top = stack.top();
-                    stack.pop();
-                    stack.push(top*curr);
+                    // int top = stack.top();
+                    // stack.pop();
+                    // stack.push(top*curr);
+                    last *= curr;
                 }else if(lastsign == '/'){
-                    int top = stack.top();
-                    stack.pop();
-                    stack.push(top/curr);
+                    // int top = stack.top();
+                    // stack.pop();
+                    // stack.push(top/curr);
+                    last /= curr;
                 }
                 lastsign = s[i];
                 curr = 0;
             }
         }
+        res += last;
         
-        while(!stack.empty())
-            res += stack.top(), stack.pop();
+        // while(!stack.empty())
+        //     res += stack.top(), stack.pop();
         return res;
     }
 };

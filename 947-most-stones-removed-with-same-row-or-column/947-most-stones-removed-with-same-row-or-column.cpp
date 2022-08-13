fc4adf27c6fb9fa1d4d67c98ae8ef8dcfb77f31a
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-13 18:39:37 +0530

    Time: 60 ms (89.68%), Space: 21.2 MB (23.97%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,22 +1,54 @@
-class Solution {
-    void dfs(int src, vector<bool>& vis, vector<vector<int>>& stones){
-        vis[src] = true;
-        for(int i=0; i<stones.size(); i++){
-            if(!vis[i] && (stones[src][0] == stones[i][0] || stones[i][1] == stones[src][1]))
-                dfs(i, vis, stones);
-        }
+class DSU{
+	vector<int> parent, size;
+public:
+	int count = 0;
+	DSU(int n) : parent(n), size(n), count(0) {
+        for(int i=0; i<n; i++)
+            make_set(i);
     }
+
+	void make_set(int v){
+		parent[v] = v;
+		size[v] = 1;
+		count++;
+	}
+
+	int find_set(int v){
+		if(v == parent[v])
+			return v;
+		return parent[v] = find_set(parent[v]);
+	}
+
+	bool union_set(int a, int b){
+		a = find_set(a);
+		b = find_set(b);
+		if(a != b){
+			if(size[a] < size[b])
+				swap(a, b);
+			parent[b] = a;
+			size[a] += size[b];
+			count--;
+			return true;
+		}
+		return false;
+	}
+};
+
+class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size(), count = 0;
-        vector<bool> vis(n, false);
+        vector<int> row(10001, -1), col(10001, -1);
+        DSU uf(n);
         
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-                dfs(i, vis, stones);
-                count++;
-            }
+        for(int i=0; i<stones.size(); i++){
+            if(row[stones[i][0]] != -1)
+                uf.union_set(row[stones[i][0]], i);
+            if(col[stones[i][1]] != -1)
+                uf.union_set(col[stones[i][1]], i);
+            row[stones[i][0]] = i;
+            col[stones[i][1]] = i;
         }
-        return stones.size() - count;
+        return stones.size() - uf.count;
     }
 };

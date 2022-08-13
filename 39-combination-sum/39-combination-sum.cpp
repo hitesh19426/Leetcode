60f5dc472093604784da7f6870b61c8843750386
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-13 14:15:04 +0530

    Time: 55 ms (31.01%), Space: 30.5 MB (24.75%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<vector<int>> helper(vector<int>& arr, int ind, int target){
+        if(target == 0)
+            return {{}};
+        if(target < 0 || ind < 0)
+            return {};
+        
+        auto with = helper(arr, ind, target - arr[ind]);
+        auto without = helper(arr, ind-1, target);
+        
+        auto res = without;
+        for(auto &seq: with){
+            seq.push_back(arr[ind]);
+            res.push_back(seq);
+        }
+        
+        return res;
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        return helper(candidates, candidates.size()-1, target);
+    }
+};

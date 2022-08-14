ed1f73c2e9773aae7cdae659396ea6394ccf4ffe
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-14 16:49:26 +0530

    Time: 246 ms (75.79%), Space: 19.6 MB (88.57%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -2,13 +2,8 @@ class Node{
 public:
     int x, y, dist;
     Node(int x, int y, int dist) : x(x), y(y), dist(dist) {}
-    
     bool operator<(const Node& other) const {
-        if(dist != other.dist)
-            return dist < other.dist;
-        if(x != other.x)
-            return x < other.x;
-        return y < other.y;
+        return dist > other.dist;
     }
 };
 
@@ -16,19 +11,20 @@ class Solution {
     bool isvalid(int x, int y, int m, int n){
         return x>=0 && y>=0 && x<m && y<n;
     }
-    
-    int dijkstra(vector<vector<int>>& grid){
-        int m = grid.size(), n = grid[0].size();
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m = heights.size(), n = heights[0].size();
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        set<Node> minheap;
+        priority_queue<Node> pq;
         
-        minheap.insert(Node(0, 0, 0));
         dist[0][0] = 0;
+        pq.push(Node(0, 0, 0));
         
-        int dir[] = {1,0,-1,0,1}, res = 0;
-        while(!minheap.empty()){
-            auto [x, y, dis] = *minheap.begin();
-            minheap.erase(minheap.begin());
+        int dir[] = {1,0,-1,0,1};
+        
+        while(!pq.empty()){
+            const auto [x, y, dis] = pq.top();
+            pq.pop();
             
             if(x == m-1 && y == n-1)
                 return dis;
@@ -36,20 +32,15 @@ class Solution {
             for(int k=0; k<4; k++){
                 int newx = x+dir[k], newy = y+dir[k+1];
                 if(isvalid(newx, newy, m, n)){
-                    int newdist = max(dist[x][y], abs(grid[x][y] - grid[newx][newy]));
-                    if(dist[newx][newy] > newdist){
-                        minheap.erase(Node(newx, newy, dist[newx][newy]));
-                        dist[newx][newy] = newdist;
-                        minheap.insert(Node(newx, newy, dist[newx][newy]));    
+                    int w = max(dis, abs(heights[newx][newy] - heights[x][y]));
+                    if(dist[newx][newy] > w){
+                        dist[newx][newy] = w;
+                        pq.push(Node(newx, newy, w));
                     }
                 }
             }
         }
-                                                 
+        
         return -1;
-    }
-public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        return dijkstra(heights);
     }
 };

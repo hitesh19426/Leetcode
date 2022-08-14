49bb6013f15d9680c11c85f71b5bd8a6f813ca2a
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-14 16:14:50 +0530

    Time: 80 ms (34.01%), Space: 13.6 MB (55.95%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> graph(n, vector<pair<int, int>>());
+        vector<vector<pair<int, int>>> graph(n);
         for(auto& flight: flights){
-            graph[flight[0]].push_back({flight[1], flight[2]});
+            graph[flight[0]].emplace_back(flight[1], flight[2]);
         }
         
-        vector<vector<int>> dist(n, vector<int>(k+2, INT_MAX));
-        queue<pair<int, int>> queue;
+        
+        vector<vector<int>> dist(n, vector<int>(k+3, INT_MAX));
+        queue<int> queue;
+        
+        queue.push(src);
         dist[src][0] = 0;
-        queue.push({src, 0});
         
         int ans = INT_MAX;
-        while(!queue.empty()){
-            auto [node, stops] = queue.front();
-            queue.pop();
-            
-            if(node == dst){
-                ans = min(ans, dist[node][stops]);
-            }
-            
-            for(auto [v, len]: graph[node]){
-                if(stops <= k && dist[v][stops+1] > dist[node][stops] + len){
-                    queue.push({v, stops+1});
-                    dist[v][stops+1] = dist[node][stops] + len;
+        for(int i=0; i<k+2; i++){
+            int size = queue.size();
+            while(size--){
+                auto node = queue.front();
+                queue.pop();
+                
+                if(node == dst)
+                    ans = min(ans, dist[node][i]);
+                
+                for(auto [v, w]: graph[node]){
+                    if(dist[v][i+1] > dist[node][i] + w){
+                        dist[v][i+1] = dist[node][i] + w;
+                        queue.push(v);
+                    }
                 }
             }
         }

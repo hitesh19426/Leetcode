2945410c2eb0318d1c9a681dd3aade74c7a55d32
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-14 16:58:25 +0530

    Time: 362 ms (37.52%), Space: 66.2 MB (65.33%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,36 +1,33 @@
 class Solution {
-    double dijkstra(int start, int end, vector<vector<pair<int, double>>>& graph){
-        int n = graph.size();
-        vector<double> dist(n, 0);
-        vector<bool> vis(n, false);
-        queue<int> queue;
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        vector<vector<pair<int, double>>> graph(n);
+        for(int i=0; i<edges.size(); i++){
+            graph[edges[i][0]].emplace_back(edges[i][1], succProb[i]);
+            graph[edges[i][1]].emplace_back(edges[i][0], succProb[i]);
+        }
         
-        queue.push(start);
+        priority_queue<pair<double, int>> pq;
+        vector<double> dist(n, 0);
+        
+        pq.push({1, start});
         dist[start] = 1;
         
-        while(!queue.empty()){
-            auto ver = queue.front();
-            queue.pop();
+        while(!pq.empty()){
+            const auto [prob, node] = pq.top();
+            pq.pop();
             
-            for(auto [v, succ]: graph[ver]){
-                double newprob = dist[ver]*succ;
-                if(dist[v] < newprob){
-                    dist[v] = newprob;
-                    queue.push(v);
+            if(node == end)
+                return prob;
+            
+            for(const auto& [v, p]: graph[node]){
+                if(dist[v] < prob*p){
+                    dist[v] = prob*p;
+                    pq.push({dist[v], v});
                 }
             }
         }
-         
-        return dist[end];
-    }
-public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<vector<pair<int, double>>> graph(n, vector<pair<int, double>>());
-        for(int i=0; i<edges.size(); i++){
-            graph[edges[i][0]].push_back({edges[i][1], succProb[i]});
-            graph[edges[i][1]].push_back({edges[i][0], succProb[i]});
-        }
         
-        return dijkstra(start, end, graph);
+        return 0;
     }
 };

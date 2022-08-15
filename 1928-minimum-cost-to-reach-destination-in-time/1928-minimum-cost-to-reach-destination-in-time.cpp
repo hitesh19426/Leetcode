9fb0504aeabcb091ff8f4f46260b3f6c65b321d4
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-15 08:38:22 +0530

    Time: 551 ms (45.11%), Space: 29.4 MB (66.35%) - LeetHub

--- 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -1,17 +1,17 @@
 class Solution {
     const int INF = 1e7;
     int dp[1001][1001];
-    int helper(int src, int maxTime, int parent, int dest, vector<int>& cost, vector<vector<pair<int, int>>>& graph){
+    int helper(int src, int maxTime, vector<int>& cost, vector<vector<pair<int, int>>>& graph){
         if(maxTime < 0)
             return INF;
-        if(src == dest)
-            return cost[dest];
+        if(src == cost.size()-1)
+            return cost[src];
         if(dp[src][maxTime] != 0)
             return dp[src][maxTime];
         
         int minCost = INF;
         for(const auto &[v, t]: graph[src])
-            minCost = min(minCost, cost[src] + helper(v, maxTime-t, src, dest, cost, graph));
+            minCost = min(minCost, cost[src] + helper(v, maxTime-t, cost, graph));
         return dp[src][maxTime] = minCost;
     }
 public:
@@ -24,7 +24,7 @@ public:
             graph[edge[1]].emplace_back(edge[0], edge[2]);
         }
         
-        auto ans = helper(0, maxTime, -1, n-1, passingFees, graph);
+        auto ans = helper(0, maxTime, passingFees, graph);
         return (ans == INF ? -1 : ans);
     }
 };

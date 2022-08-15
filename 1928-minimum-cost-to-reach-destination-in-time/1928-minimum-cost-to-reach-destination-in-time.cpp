e312cb6091ca019e9b76a4ba61181d34d4568997
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-15 08:53:51 +0530

    Time: 800 ms (27.25%), Space: 96.2 MB (11.84%) - LeetHub

--- 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -1,30 +1,30 @@
 class Solution {
     const int INF = 1e7;
-    int dp[1001][1001];
-    int helper(int src, int maxTime, vector<int>& cost, vector<vector<pair<int, int>>>& graph){
+    vector<vector<int>> dp;
+    int helper(int src, int maxTime, int parent, int dest, vector<int>& cost, vector<vector<pair<int, int>>>& graph){
         if(maxTime < 0)
             return INF;
-        if(src == cost.size()-1)
-            return cost[src];
-        if(dp[src][maxTime] != 0)
+        if(src == dest)
+            return cost[dest];
+        if(dp[src][maxTime] != -1)
             return dp[src][maxTime];
         
         int minCost = INF;
         for(const auto &[v, t]: graph[src])
-            minCost = min(minCost, cost[src] + helper(v, maxTime-t, cost, graph));
+            minCost = min(minCost, cost[src] + helper(v, maxTime-t, src, dest, cost, graph));
         return dp[src][maxTime] = minCost;
     }
 public:
     int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& passingFees) {
         int n = passingFees.size();
-        memset(dp, 0, sizeof(dp));
+        dp.assign(n, vector<int>(maxTime+1, -1));
         vector<vector<pair<int, int>>> graph(n);
         for(auto &edge: edges){
             graph[edge[0]].emplace_back(edge[1], edge[2]);
             graph[edge[1]].emplace_back(edge[0], edge[2]);
         }
         
-        auto ans = helper(0, maxTime, passingFees, graph);
+        auto ans = helper(0, maxTime, -1, n-1, passingFees, graph);
         return (ans == INF ? -1 : ans);
     }
 };

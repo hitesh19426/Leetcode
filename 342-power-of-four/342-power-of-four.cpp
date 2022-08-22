542719ea61fc661b5f5957c5005102e15b0fdeff
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-08-22 09:52:08 +0530

    Time: 7 ms (13.19%), Space: 6.1 MB (6.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -3,11 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if(n<1)
             return false;
-        int count = 0, idx = -1;
-        for(int i=0; i<31; i++){
-            if(n&(1<<i))
-                count++, idx = i;
-        }
-        return count==1 && idx%2==0;
+        int log2n = log2(n);
+        return (log2n%2 == 0 && (1<<log2n) == n);
     }
 };

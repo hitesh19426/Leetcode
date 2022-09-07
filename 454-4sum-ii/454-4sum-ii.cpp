6d8998d6f85de7607432aa99948427d7737ca539
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-09-07 15:02:50 +0530

    Time: 2398 ms (5.03%), Space: 37.9 MB (5.02%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,24 +1,33 @@
 class Solution {
+    unordered_map<int, int> helper(vector<vector<int>>& arrs, int low, int high){
+        if(high < low)
+            return unordered_map<int, int>();
+        if(low == high){
+            unordered_map<int, int> cnt;
+            for(auto& x: arrs[low])
+                cnt[x]++;
+            return cnt;
+        }
+        
+        int mid = (low+high)/2;
+        auto left = helper(arrs, low, mid);
+        auto right = helper(arrs, mid+1, high);
+        
+        unordered_map<int, int> cnt;
+        for(auto &[key1, cnt1]: left){
+            for(auto &[key2, cnt2]: right)
+                cnt[key1+key2] += cnt1*cnt2;
+        }
+        return cnt;
+    }
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        unordered_map<int, int> sum12, sum34;
-        for(int& x: nums1){
-            for(int &y: nums2)
-                sum12[x+y]++;
-        }
-        
-        for(int& x: nums3){
-            for(int &y: nums4){
-                sum34[x+y]++;
-            }
-        }
-        
-        int ans = 0;
-        for(auto &[key, cnt]: sum12){
-            if(sum34.find(-key) != sum34.end())
-                ans += cnt*sum34[-key];
-        }
-        
-        return ans;
+        vector<vector<int>> arrs{nums1, nums2, nums3, nums4};
+        auto cnt = helper(arrs, 0, arrs.size()-1);
+        return cnt[0];
     }
 };
+/*
+TC(K, N) = logK*(time for merging = K*N * K*N) = K*K*N*N*logK
+SC(K, N) = K*N
+*/

b3965dac1b6716d77ad43b3e4dce15d9c6aff57c
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-09-26 13:32:50 +0530

    Time: 13 ms (38.17%), Space: 11.3 MB (88.77%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,12 +1,14 @@
 class DSU{
-    int count = 0;
     vector<int> parent, size;
 public:
-    DSU(int n){
-        parent.resize(n);
-        size.resize(n);
+    DSU(int n) : parent(n), size(n) {
         for(int i=0; i<n; i++)
             make_set(i);
+    }
+    
+    void make_set(int v){
+        parent[v] = v;
+        size[v] = 1;
     }
     
     int find_set(int v){
@@ -15,28 +17,15 @@ public:
         return parent[v] = find_set(parent[v]);
     }
     
-    void make_set(int v){
-        parent[v] = v;
-        size[v] = 1;
-        count++;
-    }
-    
-    bool union_set(int a, int b){
+    void union_set(int a, int b){
         a = find_set(a);
         b = find_set(b);
-        if(a == b)
-            return false;
-        
-        if(size[a] < size[b])
-            swap(a, b);
-        parent[b] = a;
-        size[a] += size[b];
-        count--;
-        return true;
-    }
-    
-    int get_count(){
-        return count;
+        if(a != b){
+            if(size[a] < size[b])
+                swap(a, b);
+            parent[b] = a;
+            size[a] += size[b];
+        }
     }
 };
 
@@ -47,16 +36,15 @@ public:
             return a[1] == '=';
         });
         
-        DSU dsu(26);
-        for(auto eq: equations){
-            if(eq[1] == '!'){
-                if(dsu.find_set(eq[0]-'a') == dsu.find_set(eq[3]-'a'))
+        DSU uf(26);
+        for(auto& eq: equations){
+            if(eq[1] == '='){
+                uf.union_set(eq[0]-'a', eq[3]-'a');
+            }else{
+                if(uf.find_set(eq[0]-'a') == uf.find_set(eq[3]-'a'))
                     return false;
             }
-            else
-                dsu.union_set(eq[0]-'a', eq[3]-'a');
         }
-        
         return true;
     }
 };

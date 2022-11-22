5c7c1a83345866b3c70c3463d2189653f6768df9
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-11-22 14:29:53 +0530

    Time: 1911 ms (5.00%), Space: 153.5 MB (7.04%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> sq;
-        for(int i=1; i*i<=n; i++){
-            sq.push_back(i*i);
-        }
+        int sq = 1;
+        while(sq*sq <= n)
+            sq++;
+        sq--;
         
-        vector<vector<int>> pos(sq.size()+1, vector<int>(n+1, INT_MAX));
+        vector<vector<int>> pos(sq+1, vector<int>(n+1, INT_MAX));
         pos[0][0] = 0;
         
-        for(int i=1; i<=sq.size(); i++){
+        for(int i=1; i<=sq; i++){
             pos[i][0] = 0;
             for(int j=1; j<=n; j++){
                 pos[i][j] = pos[i-1][j];
-                if(j>=sq[i-1] && pos[i][j-sq[i-1]] != INT_MAX)
-                    pos[i][j] = min(pos[i][j], 1+pos[i][j-sq[i-1]]);
+                if(j>=i*i && pos[i][j-i*i] != INT_MAX)
+                    pos[i][j] = min(pos[i][j], 1+pos[i][j-i*i]);
             }
         }
         
-        return pos[sq.size()][n];
+        return pos[sq][n];
     }
 };

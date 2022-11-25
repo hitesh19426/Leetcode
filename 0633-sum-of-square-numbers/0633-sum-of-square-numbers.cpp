382e704303c2995538125b570f90877b1131caa2
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-11-25 17:18:29 +0530

    Time: 7 ms (39.70%), Space: 6 MB (25.78%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,15 +1,9 @@
 class Solution {
-    set<long long> sq;
 public:
-    Solution(){
-        const long long n = (1ll<<31ll)-1;
-        for(long long i=0; i*i<=n; i++){
-            sq.insert(i*i);
-        }
-    }
     bool judgeSquareSum(int c) {
         for(long long i=0; i*i<=c; i++){
-            if(sq.find(c-i*i) != sq.end())
+            long long b = sqrt(c-i*i);
+            if(b*b == c-i*i)
                 return true;
         }
         return false;

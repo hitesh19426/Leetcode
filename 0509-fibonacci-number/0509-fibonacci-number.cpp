3d69abf1e4e8f5cfcefd3d89aa0cc00a8b78e803
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2022-11-27 10:26:23 +0530

    Time: 0 ms (100.00%), Space: 6 MB (38.27%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)
+            return n;
+        
+        int fib[n+1];
+        fib[0] = 0, fib[1] = 1;
+        for(int i=2; i<=n; i++)
+            fib[i] = fib[i-1] + fib[i-2];
+        return fib[n];
+    }
+};
